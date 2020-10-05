554af3dad2d77e8e24e74a0aa1a1b6d94ecc0e50
rafal <rafal1behrendt@gmail.com>
2020-10-05 19:59:30 +0200

    quick sort done

--- sortingMethods.py
+++ sortingMethods.py
@@ -12,4 +12,21 @@ def insertionSort(unorderedArr, orderedArr=[]):
     return insertionSort(unorderedArr, insertItem(orderedArr))
 
 
+def quickSort(array):
+    if len(array) < 2: return array
+    pivot = array.pop(0)
+
+    def partition(arr, smaller, bigger):
+        if not arr:
+            return smaller, bigger
+        elif arr[0] <= pivot:
+            return partition(arr[1:], smaller + [arr[0]], bigger)
+        else:
+            return partition(arr[1:], smaller, bigger + [arr[0]])
+
+    smaller, bigger = partition(array, [], [])
+    return quickSort(smaller) + [pivot] + quickSort(bigger)
+
+
 print('insertion sort: ', insertionSort([0, -1, 0, 9.2, 12, -22.5, 3, 9.9]))
+print('quick sort: ', quickSort([0, -55.9, -55.8, 9.2, 2, 0, 3.3, 3.3, 9.11]))
