939acdee765aeee958eeaa214c7bd3c4d7cc23dc
Silkypaladin <bednarski.szymon99@gmail.com>
2020-10-05 14:54:24 +0200

    Implement bubble sort algorithm, add examples

--- sorts.py
+++ sorts.py
@@ -8,3 +8,22 @@ def insert_sort(arr):
             j -= 1
         arr[j+1] = next_item
         
+def bubble_sort(arr):
+    swapped = True
+
+    while swapped:
+        swapped = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = True
+
+arr = [5,4,2,5,1,1]
+arr2 = [4,2,1,11,23,45,21]
+bubble_sort(arr)
+insert_sort(arr2)
+print(arr)
+print(arr2)
+                
+                
+            
