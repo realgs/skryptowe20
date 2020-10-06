c08b4f7b3632590a1b40997db2cfd19e29b6b516
Piotr Rasiński <piotrrasinski@MacBook-Pro-Piotr.local>
2020-10-06 20:26:50 +0200

    Implemented insertion sort algorythm

--- sorts.py
+++ sorts.py
@@ -20,7 +20,23 @@ def quick_sort(arr, low, high):
     return arr
 
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+    return arr
+
+
 array_1 = [2, 1, 1, 5, 81, 2, 19]
+array_2 = [13.4, 9.8, 8.7, 11.2, 92.9, 54.4, 11.2]
 
 print('Sorted array with quicksort:')
 print(quick_sort(array_1, 0, len(array_1)-1))
+print('Sorted array with insertionsort')
+print(insertion_sort(array_2))
