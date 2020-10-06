e1d435e4e470ce68d2b67242c035a490f5385de3
Grzegorz Myjak <g-myjak@wp.pl>
2020-10-06 23:56:37 +0200

    Implemented quick sort function from PL wikipedia pseudocode

--- Lista1/sortingalgorithms.py
+++ Lista1/sortingalgorithms.py
@@ -18,8 +18,35 @@ def bubble_sort(array):
             iteration_count -= 1
 
 
+def quick_sort(array):
+    def helper(left, right):
+        if left < right:
+            pivot = divide_array(left, right)
+            helper(left, pivot-1)
+            helper(pivot+1, right)
+
+    def divide_array(left, right):
+        pivot_index = left + (right - left) // 2
+        pivot_value = array[pivot_index]
+        swap(array, pivot_index, right)
+        current_position = left
+        for index in range(left, right):
+            if array[index] < pivot_value:
+                swap(array, index, current_position)
+                current_position += 1
+        swap(array, current_position, right)
+        return current_position
+
+    helper(0, len(array)-1)
+
+
 if __name__ == "__main__":
     test_array_1 = [5, 2, 6, 1, 9, 3, 8, 7, 4]
-    print("test array:", test_array_1)
+    print("Test array:", test_array_1)
     bubble_sort(test_array_1)
-    print("Test array after bubble sort:", test_array_1)
+    print("Test array after bubble sort:", test_array_1, "\n")
+
+    test_array_2 = [5, 2, 6, 1, 9, 3, 8, 7, 4]
+    print("Test array:", test_array_2)
+    bubble_sort(test_array_2)
+    print("Test array after quick sort:", test_array_2, "\n")
