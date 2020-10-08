8476bc1b1776da3240c8405d7f8a5cc658f78831
pbeat8 <61192446+pbeat8@users.noreply.github.com>
2020-10-08 13:16:54 +0200

    Create bubble_sort.py
    Bubblesort program

---
+++ bubble_sort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+unsorted = []
+
+n = int(input("Wpisz ilość elementów: "))
+
+print("Wpisz elementy: ")
+for i in range(0, n):
+    unsorted.append(int(input()))
+
+changed = True
+while changed:
+    changed = False
+    for i in range(1, n):
+        if unsorted[i] < unsorted[i - 1]:
+            unsorted[i], unsorted[i - 1] = unsorted[i - 1], unsorted[i]
+            changed = True
+
+print(unsorted)
