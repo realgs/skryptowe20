8efe0db04a883053ff6e5ee13bc22c882fab9d6e
japko-7 <filipjablonski@icloud.com>
2020-10-08 19:57:25 +0200

    extended sortiing tests and .gitgnore

--- sorting_methods/main.py
+++ sorting_methods/main.py
@@ -4,16 +4,31 @@ from algorithms import *
 import random
 
 SIZE = 100
-# array = [292, 1, 2, 20, 15, 10, 2, 2, 1, 4, 100, 7, 0]
 
 def generate_random_array(size):
     array = []
     for x in range(size): array.append(random.randint(0, size))
     return array
 
+def generate_ascending_array(size):
+    return [*range(size)]
+
+def generate_descending_array(size):
+    return [*range(size, 0, -1)]
+
 
 if __name__ == "__main__":
     array = generate_random_array(SIZE)
     print("b4:", array)
+    print("\ninsert:", insert.sort(array.copy()))
+    print("\nquick:", quick.sort(array.copy()))
+
+    array = generate_ascending_array(SIZE)
+    print("\nb4:", array)
+    print("\ninsert:", insert.sort(array.copy()))
+    print("\nquick:", quick.sort(array.copy()))
+
+    array = generate_descending_array(SIZE)
+    print("\nb4:", array)
     print("\ninsert:", insert.sort(array.copy()))
     print("\nquick:", quick.sort(array.copy()))
