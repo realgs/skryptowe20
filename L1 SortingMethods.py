320d564051ddf6466b73acf60a6101b06f5e01f0
Michal <michal.chrobot@op.pl>
2020-10-08 16:15:19 +0200

    QuickSort added

--- L1 SortingMethods.py
+++ L1 SortingMethods.py
@@ -1,4 +1,4 @@
-def BubbleSort(tab):
+def bubble_sort(tab):
     for i in range(len(tab)):
         j = len(tab) - 1
         while j > i:
@@ -8,3 +8,23 @@ def BubbleSort(tab):
                 tab[j - 1] = tmp
             j -= 1
     return tab
+
+
+def quick_sort_partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pi = quick_sort_partition(arr, low, high)
+        quick_sort_partition(arr, low, pi - 1)
+        quick_sort_partition(arr, pi + 1, high)
