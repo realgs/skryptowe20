1379e20960a1cc696c78627165fbe58682f3eca1
DamianBartosz <damian.mateusz.bartosz@gmail.com>
2020-10-10 00:13:40 +0200

    Define quickSort method

--- SortingMethods.py
+++ SortingMethods.py
@@ -1,26 +1,46 @@
+def checkList(listOfNumbers):
+    if not isinstance(listOfNumbers, list):
+        return False
+    if not all(isinstance(elem, int) or isinstance(elem, float) for elem in listOfNumbers):
+        return False
+    return True
+
+
 def bubbleSort(listOfNumbers):
     if not checkList(listOfNumbers):
         return False
     for i in range(len(listOfNumbers)):
         swapped = False
         for j in range(len(listOfNumbers) - i - 1):
-            if listOfNumbers[j] > listOfNumbers[j+1]:
-                (listOfNumbers[j], listOfNumbers[j+1]) = (listOfNumbers[j+1], listOfNumbers[j])
+            if listOfNumbers[j] > listOfNumbers[j + 1]:
+                (listOfNumbers[j], listOfNumbers[j + 1]) = (listOfNumbers[j + 1], listOfNumbers[j])
                 swapped = True
         if not swapped:
             return True
     return True
 
 
+def quickSortR(listOfNumbers, beg, end):
+    if beg < end:
+        pivot = listOfNumbers[beg]
+        border = beg + 1
+        index = border
+
+        while index <= end:
+            if (listOfNumbers[index] < pivot):
+                (listOfNumbers[border], listOfNumbers[index]) = (listOfNumbers[index], listOfNumbers[border])
+                border+=1
+            index+=1
+        border-=1
+        (listOfNumbers[border], listOfNumbers[beg]) = (listOfNumbers[beg], listOfNumbers[border])
+        quickSortR(listOfNumbers, beg, border-1)
+        quickSortR(listOfNumbers, border+1, end)
+
+
 def quickSort(listOfNumbers):
-    pass
-
-
-def checkList(listOfNumbers):
-    if not isinstance(listOfNumbers, list):
+    if not checkList(listOfNumbers):
         return False
-    if not all(isinstance(elem, int) or isinstance(elem, float) for elem in listOfNumbers):
-        return False
+    quickSortR(listOfNumbers, 0, len(listOfNumbers) - 1)
     return True
 
 
