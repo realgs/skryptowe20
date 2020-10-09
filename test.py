3d4c91651a1ea8b188eac3ba65d8909c59ec7785
limisie <k.limisiewicz@gmail.com>
2020-10-09 21:21:00 +0200

    Add bubble sort test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
-
 from mergesort import merge_sort
+from bubblesort import bubble_sort
 
 test_case_1 = [3, 12, -3, 0, 324, 11, 2, 7, 2, 9]
 test_case_2 = []
@@ -21,6 +21,15 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(merge_sort(test_case_5), [1, 1.1])
         self.assertEqual(merge_sort(test_case_6), [1000])
         self.assertEqual(merge_sort(test_case_7), [0, 0, 0])
+
+    def test_bubble_sort(self):
+        self.assertEqual(bubble_sort(test_case_1), [-3, 0, 2, 2, 3, 7, 9, 11, 12, 324])
+        self.assertEqual(bubble_sort(test_case_2), [])
+        self.assertEqual(bubble_sort(test_case_3), [0])
+        self.assertEqual(bubble_sort(test_case_4), [-1, 1, 1])
+        self.assertEqual(bubble_sort(test_case_5), [1, 1.1])
+        self.assertEqual(bubble_sort(test_case_6), [1000])
+        self.assertEqual(bubble_sort(test_case_7), [0, 0, 0])
 
 
 if __name__ == '__main__':
