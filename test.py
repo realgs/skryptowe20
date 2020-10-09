e0cd896841531edd810b4a0914f7d2307cc6fb7a
limisie <k.limisiewicz@gmail.com>
2020-10-09 20:54:57 +0200

    Add sorting unit test

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from mergesort import merge_sort
+
+test_case_1 = [3, 12, -3, 0, 324, 11, 2, 7, 2, 9]
+test_case_2 = []
+test_case_3 = [0]
+test_case_4 = [1, -1, 1]
+test_case_5 = [1.1, 1]
+test_case_6 = [1000]
+test_case_7 = [0, 0, 0]
+
+
+class TestSortingMethods(unittest.TestCase):
+
+    def test_merge_sort(self):
+        self.assertEqual(merge_sort(test_case_1), [-3, 0, 2, 2, 3, 7, 9, 11, 12, 324])
+        self.assertEqual(merge_sort(test_case_2), [])
+        self.assertEqual(merge_sort(test_case_3), [0])
+        self.assertEqual(merge_sort(test_case_4), [-1, 1, 1])
+        self.assertEqual(merge_sort(test_case_5), [1, 1.1])
+        self.assertEqual(merge_sort(test_case_6), [1000])
+        self.assertEqual(merge_sort(test_case_7), [0, 0, 0])
+
+
+if __name__ == '__main__':
+    unittest.main()
