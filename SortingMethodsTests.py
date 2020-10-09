343597b35613f00e52aeb41ccfc2a1e5da56566d
DamianBartosz <damian.mateusz.bartosz@gmail.com>
2020-10-09 13:05:40 +0200

    Add testBubbleOneElement method
    Create method for bubbleSort testing with list of one element
    Add checking result of bubbleSort method in testBubbleCorrect method

--- SortingMethodsTests.py
+++ SortingMethodsTests.py
@@ -5,7 +5,7 @@ from SortingMethods import bubbleSort, quickSort
 class SortingMethodsTestCase(unittest.TestCase):
     def testBubbleCorrect(self):
         testList = [0, -20, 3, 526, -464, -65, 235, 6, 66, -4]
-        bubbleSort(testList)
+        self.assertTrue(bubbleSort(testList))
         self.assertEqual(testList, [-464, -65, -20, -4, 0, 3, 6, 66, 235, 526])
 
     def testBubbleEmpty(self):
@@ -13,6 +13,11 @@ class SortingMethodsTestCase(unittest.TestCase):
         self.assertTrue(bubbleSort(testList))
         self.assertEqual(testList, [])
 
+    def testBubbleOneElement(self):
+        testList = [1]
+        self.assertTrue(bubbleSort(testList))
+        self.assertEqual(testList, [1])
 
+    
 if __name__ == '__main__':
     unittest.main()
