e9402996bf6370283c2aea18391ca7d016210f44
DamianBartosz <damian.mateusz.bartosz@gmail.com>
2020-10-09 13:28:43 +0200

    Add QuickSortTestCase with all testing methods

--- SortingMethodsTests.py
+++ SortingMethodsTests.py
@@ -29,5 +29,32 @@ class BubbleSortTestCase(unittest.TestCase):
         self.assertFalse(bubbleSort(testList))
 
 
+class QuickSortTestCase(unittest.TestCase):
+    def testQuickCorrect(self):
+        testList = [0, -20.52, 3, 526, -464.5, -65, 235, 6, 66.947, -4]
+        self.assertTrue(quickSort(testList))
+        self.assertEqual(testList, [-464.5, -65, -20.52, -4, 0, 3, 6, 66.947, 235, 526])
+
+    def testQuickEmpty(self):
+        testList = []
+        self.assertTrue(quickSort(testList))
+        self.assertEqual(testList, [])
+
+    def testQuickOneElement(self):
+        testList = [1]
+        self.assertTrue(quickSort(testList))
+        self.assertEqual(testList, [1])
+
+    def testQuickNotNumericElementsInList(self):
+        testList = [1, "adfa", 523, False, [2, 4, 5], 3]
+        self.assertFalse(quickSort(testList))
+
+        testList = [True, [12], 2]
+        self.assertFalse(quickSort(testList))
+
+        testList = [[52], 2, "sadf"]
+        self.assertFalse(quickSort(testList))
+
+
 if __name__ == '__main__':
     unittest.main()
