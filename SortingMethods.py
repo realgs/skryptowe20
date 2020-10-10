2cfe153ac1a604696e78e595ccacb672237bd8a2
Robert <robertpawelh@gmail.com>
2020-10-10 17:39:28 +0200

    Add new test cases

--- SortingMethods.py
+++ SortingMethods.py
@@ -58,12 +58,14 @@ def quick_sort(list_to_sort):
 def do_tests(test_lists):
     sort_algorithms = {'MergeSort': merge_sort, 'QuickSort': quick_sort}
     for name, sort_algorithm in sort_algorithms.items():
-        for nr, test_list in enumerate(test_lists):
+        for test_list in test_lists:
             test_list_copy = test_list.copy()
             sort_algorithm(test_list_copy)
-            print(f'Test listy nr {nr}, algorytmem {name}: ', test_list_copy)
+            print(f'{test_list} po posortowaniu algorytmem {name}: ', test_list_copy)
+        print()
 
 if __name__ == '__main__':
-    test_lists = [[9,8,7,6,5,4,3,2,1], [4,7,1,2,8,6,3,9,5], [1,2,3,6,5,4,7,9,8]]
+    test_lists = [[9,8,7,6,5,4,3,2,1], [4,7,0,2,8,6,3,9,5], [1,2,3,4,5,6,7,8,9],
+                  [-2,6,7.5,3,0,-2,-18], [1,1,1], [], [1.3,2.6,0.1,0,-2.3]]
     do_tests(test_lists)
 
