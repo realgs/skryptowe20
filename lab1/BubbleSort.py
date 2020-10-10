7ec74f176f040cabb957113d572274f15eb82c5d
= <38375497+amsok@users.noreply.github.com>
2020-10-10 16:05:51 +0200

    BubbleSort introduced

---
+++ lab1/BubbleSort.py
@@ -0,0 +1,5 @@
+def BubbleSort(tab):
+    for lap in range(len(tab)-1,0,-1):
+        for i in range(lap):
+            if tab[i] > tab[i+1]:
+                tab[i],tab[i+1] = tab[i+1], tab[i]
---
+++ lab1/Main.py
@@ -0,0 +1,8 @@
+import random
+from BubbleSort import BubbleSort
+
+
+testTable = [random.randint(-1000,1000) for i in range(15)]
+print(testTab)
+BubbleSort(testTab)
+print(testTab)
