0a510aeccea77091e80fe6479ca60ec4268d217d
KeterSCP <twinker3228@gmail.com>
2020-10-11 13:34:21 +0300

    Modified gitignore, added solution for 1st task

---
+++ lab2/EnvVars/EnvVars/KodPowrotu.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool TryParseInt(const char* val, int& out)
+{
+    try
+    {
+        out = stoi(val);
+        return true;
+    }
+    catch (const invalid_argument&)
+    {
+        return false;
+    }
+}
+
+void PrintCode(int code, bool isSilent)
+{
+    if (!isSilent)
+    {
+        cout << code << endl;
+    }
+}
+
+int main(int argc, char** argv, char** env)
+{
+    bool is_silent = strcmp(argv[1], "/S") == 0 || strcmp(argv[1], "/s") == 0;
+
+    if (is_silent)
+    {
+        --argc;
+    }
+
+    int number;
+
+    if (argc == 1)
+    {
+        PrintCode(11, is_silent);
+        return 11;
+    }
+    else if (argc > 2)
+    {
+        PrintCode(13, is_silent);
+        return 13;
+    }
+    else
+    {
+        if (TryParseInt(argv[1], number))
+        {
+            PrintCode(number, is_silent);
+            return number;
+        }
+        PrintCode(12, is_silent);
+        return 12;
+    }
+}
+
