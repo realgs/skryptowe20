d1498f7a0f19ded5f8a024aeb8eff73528f69f58
szawruk <szawruk@gmail.com>
2020-10-12 03:08:17 +0200

    zad3 upd and cmd script added

--- lab2/rozwiazanie/PokazPodobne.cpp
+++ lab2/rozwiazanie/PokazPodobne.cpp
@@ -2,55 +2,113 @@
 #include <string.h>
 #include "iostream"
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <cctype>
+#include <string>
 using namespace std;
 
-bool containsName(string env)
+bool silentMode = false;
+char asciitolower(char in) {
+    if (in <= 'Z' && in >= 'A')
+        return in - ('Z' - 'z');
+    return in;
+}
+bool containsName(string name, string substring)
 {
+    std::transform(name.begin(), name.end(), name.begin(), asciitolower);
+    std::transform(substring.begin(), substring.end(), substring.begin(), asciitolower);
+    if (name.find(substring) != string::npos)
+        return true;
+    return false;
     return true;
 }
 
-int main(int argc, char *argv[], char **envp)
+bool isSilentSwitch(char arg[])
 {
-    for (char **env = envp; *env != 0; env++)
-    {
-    char *thisEnv = *env;
-    string tenv = thisEnv;
+    if((string)arg == "/s" || (string)arg == "/S")
+        return true;
+    return false;
+}
 
+vector <string> split(string line, char separator)
+{
     vector <string> tokens;
-    stringstream check1(tenv);
+    stringstream strstream(line);
     string intermediate;
 
-    while(getline(check1, intermediate, '='))
+    while(getline(strstream, intermediate, separator))
     {
         tokens.push_back(intermediate);
     }
+    return tokens;
+}
 
-    if(containsName(tokens[0]))
+
+int main(int argc, char *argv[], char **envp)
+{
+    for(int i = 1; i < argc; i++)
     {
-        cout << tokens[0] << endl;
-        cout << "=" << endl;
-
-        string oneLine = tokens[1];
-
-        // Vector of string to save tokens
-        vector <string> tokens2;
-
-        // stringstream class check1
-        stringstream check2(oneLine);
-
-        string intermediate2;
-
-        // Tokenizing w.r.t. space ' '
-        while(getline(check2, intermediate2, ';'))
+        if(isSilentSwitch(argv[i]))
         {
-            tokens2.push_back(intermediate2);
+            silentMode = true;
+            break;
+        }
+    }
+
+    vector<pair<string, vector<string> > > envVariables;
+    for (char **env = envp; *env != 0; env++)
+    {
+        vector<string> envVariable = split(*env, '=');
+        envVariables.push_back(make_pair(envVariable[0], split( envVariable[1], ';')));
+    }
+
+    if(!silentMode)
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            bool variableContainsParam = false;
+            for(int y = 0; y < envVariables.size(); y++)
+            {
+                if(containsName(envVariables[y].first, (string)argv[i]))
+                {
+                    variableContainsParam = true;
+                    break;
+                }
+            }
+            if(!variableContainsParam)
+            {
+                cout<<argv[i]<<"=NONE"<<endl<<endl;
+            }
+        }
+    }
+
+    for(int i = 0; i < envVariables.size(); i++)
+    {
+        if(argc == 1)
+        {
+            cout<<envVariables[i].first<<endl;
+            cout<<'='<<endl;
+            for(int z = 0; z < envVariables[i].second.size(); z++)
+                    cout<<envVariables[i].second[z]<<endl;
+            cout << endl;
+        }
+        else
+        {
+            for(int y = 1; y < argc; y++)
+            {
+                if(containsName(envVariables[i].first, (string)argv[y]))
+                {
+                    cout<<envVariables[i].first<<endl;
+                    cout<<'='<<endl;
+                    for(int z = 0; z < envVariables[i].second.size(); z++)
+                        cout<<envVariables[i].second[z]<<endl;
+                    cout << endl;
+                    break;
+                }
+            }
         }
 
-        // Printing the token vector
-        for(int i = 0; i < tokens2.size(); i++)
-            cout << tokens2[i] << '\n';
     }
-    cout << endl;
-}
+
 }
 
