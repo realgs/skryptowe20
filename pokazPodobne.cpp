4ae7470ab155ca5875f066777f45eaad950791a1
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-13 18:40:03 +0200

    Updated main function and added checkIfSilent method

--- pokazPodobne.cpp
+++ pokazPodobne.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
-main() {
+bool checkIfSilent(char* args[], int length) {
+    std::string str;
+    for (int i = 0; i < length; i++) {
+        str = args[i];
+        if (str == "-s" || str == "-S") {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(int argc, char** argv, char** envp) {
+
     return 0;
 }
