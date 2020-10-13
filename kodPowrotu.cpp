13ad7177d48f88877666c320a42dd0d838fe1496
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-13 17:58:24 +0200

    Added isDigit method and updated main

--- kodPowrotu.cpp
+++ kodPowrotu.cpp
@@ -11,18 +11,33 @@ bool checkIfSilent(char *args[], int length) {
     return false;
 }
 
+bool isDigit(char* value) {
+    try {
+        int val = std::stoi(value);
+        return (val >= 0 && val <= 9);
+    } catch (...) {
+        return false;
+    }
+}
+
 int main(int argc, char** argv) {
+    int pos = 1;
     int returnCode;
     bool isSilent = checkIfSilent(argv, argc);
 
     if (isSilent) {
         argc--;
+        pos++;
     }
 
     if (argc == 1) {
         returnCode = 11;
     } else if (argc == 2) {
-        
+        if (isDigit(argv[pos])) {
+            returnCode = atoi(argv[pos]);
+        } else {
+            returnCode = 12;
+        }
     } else {
         returnCode = 13;
     }
