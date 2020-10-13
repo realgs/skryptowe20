4f5fdc97147bccfa4cae0ce97fa51c379f94df61
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-13 22:28:45 +0200

    Added two main loops

--- pokazPodobne.cpp
+++ pokazPodobne.cpp
@@ -11,7 +11,37 @@ bool checkIfSilent(char* args[], int length) {
     return false;
 }
 
-int main(int argc, char** argv, char** envp) {
+void printVariable(std::string &variableName, std::string &variableData, char del = ':') {
 
+}
+
+int main(int argc, char **argv, char **envp) {
+    int pos = 1;
+    char delimiter = '=';
+    bool isSilent = checkIfSilent(argv, argc);
+    bool isFound;
+
+    if (isSilent) {
+        pos++;
+    }
+
+    for (int i = pos; i < argc; i++) {
+        isFound = false;
+        std::string arg(argv[i]);
+        while (*envp) {
+            std::string val(*envp++);
+            size_t index = val.find(delimiter);
+            std::string varName = val.substr(0, index);
+            val.erase(0, index + 1);
+            if (varName.find(arg) != std::string::npos) {
+                printVariable(varName, val);
+                isFound = true;
+            }
+        }
+
+        if (!isFound && !isSilent) {
+            std::cout << "parametr = NONE" << std::endl;
+        }
+    }
     return 0;
 }
