e92de55df2ef8af12ba64ec5e8cd4c8d4be2b955
Silkypaladin <bednarski.szymon99@gmail.com>
2020-10-14 15:29:55 +0200

    Implement exercise 1

---
+++ KodPowrotu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const NO_PARAMETER = 11;
+const NOT_A_NUMBER = 12;
+const MORE_THAN_TWO_ARGS = 13;
+
+bool isDigit(char c) {
+    int num = (int) c;
+    if (num > 47 && num < 58) return true;
+    return false;
+}
+
+int main(int argc, char *argv[]) {
+    bool silent_mode = false;
+    for (int i = 0; i < argc; i++) {
+        string str = argv[i];
+        if (str.compare("/s") == 0 || str.compare("/S") == 0) {
+            silent_mode = true;
+            argc--;
+        } 
+    }
+    if (argc == 1) {
+        if (!silent_mode) cout << NO_PARAMETER;
+        return NO_PARAMETER;
+    }
+    if (argc > 2) {
+        if (!silent_mode) cout << MORE_THAN_TWO_ARGS;
+        return MORE_THAN_TWO_ARGS;
+    }
+    int i = 0;
+    if (!isDigit(argv[1][0]) || (isDigit(argv[1][0]) && argv[1][1] != NULL)) {
+        if (!silent_mode) cout << NOT_A_NUMBER;
+        return NOT_A_NUMBER;
+    }
+    while (argv[1][i] != NULL) {
+        if (!isDigit(argv[1][i])) {
+            if (!silent_mode) cout << NOT_A_NUMBER;
+            return NOT_A_NUMBER;
+        }
+        i++;
+    }
+    char ret = *argv[1];
+    if (!silent_mode) cout << ret;
+    return ret;
+}
