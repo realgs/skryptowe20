6dc4213ac0f7def64d9e3295d61021818698c9ba
rafal <rafal1behrendt@gmail.com>
2020-10-15 20:50:35 +0200

    task 2 done

---
+++ PokazWszystkie.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[], char* env[])
+{
+
+	for (int i = 0; i < argc; i++)
+	{
+		std::cout <<(argv[i]) << "\n";
+	}
+
+	while (*env != NULL)
+	{
+		std::cout << *env++ << "\n";
+	}
+	return 0;
+}
