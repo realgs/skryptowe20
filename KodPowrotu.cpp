67a5128095157e1483eb47b988be7c140f6ab89f
ShakiMan <shakiman666@gmai.com>
2020-10-16 19:20:30 +0200

    Add first task

---
+++ KodPowrotu.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    bool isSilent = false;
+    if (argc >= 2)
+    {
+        for (int i = 1; i < argc; i++) {
+            if ((argv[i][0] == '/' && argv[i][1] == 's') ||
+                (argv[i][0] == '/' && argv[i][1] == 'S')) {
+                isSilent = true;
+            }
+        }
+    }
+    int x;
+    if (argc == 1) {
+        if (!isSilent)
+        {
+            printf("Kod powrotu: %d\n", 11);
+        }
+        return 11;
+    }
+    else if (argc > 2) {
+        if (!isSilent)
+        {
+            printf("Kod powrotu: %d\n", 13);
+        }
+        return 13;
+    }
+    try {
+        x = stoi(argv[1]);
+        if (argv[1][1] != NULL) {
+            x = 12;
+        }
+    }
+    catch (exception e) {
+        x = 12;
+    }
+    if (!isSilent) {
+        printf("Kod powrotu: %d\n", x);
+    }
+    return x;
+}
