cff7536b476f3f0db96cd3deb2f366e7019bb8a3
Konrad <konrad.hajduga@gmail.com>
2020-10-16 10:47:53 +0200

    add second task

---
+++ PokazWszystkie.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[], char* envp[])
+{
+    for (int i = 0; i < argc; i++) {
+        std::cout << "argv["<< i << "] = " << argv[i] << "\n";
+    }
+
+    int index = 0;
+    for (; *envp; envp++) {
+        std::cout << "envp[" << index++ << "] = " << * envp << "\n";
+    }
+}
