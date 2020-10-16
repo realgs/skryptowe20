d2745c88c4aeeec493ded44d4d99e8660c8dbf45
ShakiMan <shakiman666@gmai.com>
2020-10-16 19:23:48 +0200

    Add second task

---
+++ PokazWszystkie.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[], char* env[])
+{
+    printf("Arugemtny programu:\n\n");
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+    printf("\n\nZmienne srodowiskowe:\n\n");
+    while (*env != NULL)
+    {
+        printf("%s\n", *env++);
+    }
+    return 0;
+}
+
