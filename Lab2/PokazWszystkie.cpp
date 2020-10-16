76d16b16af1e05fcfaacc1e2b2edd4886638e4b0
spieta <spieta2@gmail.com>
2020-10-16 07:51:40 +0200

    Add PokazWszystkie

---
+++ Lab2/PokazWszystkie.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+void print(char** to_print)
+{
+    for (char** str = to_print; *str != 0; str++)
+        printf("%s\n", *str);
+}
+
+int main(int argc, char** argv, char** envp)
+{
+    print(argv);
+    print(envp);
+    return 0;
+}
