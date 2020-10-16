f4d120454ffb33f885e66f6e3f765b79a5b72b70
Kamil <kogol99@gmail.com>
2020-10-16 14:29:27 +0200

    add PokazWszystkie

---
+++ lab2/PokazWszystkie.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+    system("set");
+    for (int i = 1; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+}
