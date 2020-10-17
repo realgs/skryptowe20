78f0d6b0296b657c7179799cc37dc923185118e3
Dzejcek <jac.mikaa@gmail.com>
2020-10-17 14:21:31 +0200

    add task 3

---
+++ PokazPodobne.cpp
@@ -0,0 +1,43 @@
+﻿#include <regex>
+#include <string>
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv[], char* env[])
+{
+
+    regex findingVariables;
+    vector<string> variables;
+    bool silentMode = false;
+    for (int i = 1; i < argc; i++) {
+        int counter = 0;
+        findingVariables = argv[i];
+        while (env[counter] != NULL) {
+            if (env[counter] == "/s" || env[counter] == "/S")
+                silentMode = true;
+            if (regex_search(env[counter], findingVariables)) {
+                variables.push_back(env[counter]);
+            }
+            counter++;
+        }
+    }
+    if (variables.size() == 0 && silentMode == false) {
+        for (int i = 1; i < argc; i++) {
+            cout << argv[i] << " = " << "NONE \n";
+        }
+
+        return 0;
+    }
+    for (int i = 0; i < variables.size(); i++) {
+        string temp(variables[i]);
+        temp.append(";");
+        int sign_pos = temp.find('=');
+        cout << temp.substr(0, sign_pos) << endl << '=' << endl;
+        do
+        {
+            int semicolnPos = temp.find(';');
+            cout << temp.substr(0, semicolnPos) << endl;
+            temp = temp.substr(semicolnPos + 1);
+
+        } while (temp.find(';') != -1);
+    }
+}
