ccd49c23c50c4df13735831ae1cd5848ed115477
joannasroka <joannasroka98@gmail.com>
2020-10-17 22:19:09 +0200

    created script for KodPowrotu

---
+++ lab2_solutions/KodPowrotu_cmd/KodPowrotu.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+SET parameters=%*
+
+KodPowrotu.exe /s %parameters%
+
+IF %ERRORLEVEL% GEQ 0 IF %ERRORLEVEL% LSS 10 (
+  ECHO Przekazano: prawidlowa wartosc %parameters%
+  
+) ELSE IF %ERRORLEVEL% EQU 11 (
+  ECHO Brak parametrow
+
+) ELSE IF %ERRORLEVEL% EQU 12 (
+  ECHO Parametr %parameters% nie jest cyfra
+
+) ELSE IF %ERRORLEVEL% EQU 13 (
+  ECHO Niewlasciwa wartosc parametru %parameters%
+) 
