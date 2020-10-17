ba9ba2991d93bbddebee0b88081a0c1fce5f244c
Grzegorz Myjak <g-myjak@wp.pl>
2020-10-17 18:43:58 +0200

    Added PokazWszystkie app code

---
+++ Lista2/Lista2/PokazWszystkie.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[], char** envp)
+{
+	// Environmental variables
+	int index = 0;
+	while (envp[index] != 0)
+	{
+		cout << envp[index] << endl;
+		index++;
+	}
+
+	// Program params
+	for (int i = 0; i < argc; i++)
+	{
+		cout << argv[i] << endl;
+	}
+}
