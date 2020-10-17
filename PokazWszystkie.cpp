081778f3c3c2ef0747dcf279ec5ee9084f379ed7
Dzejcek <jac.mikaa@gmail.com>
2020-10-17 14:21:13 +0200

    add task 2

---
+++ PokazWszystkie.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main(int argc, char* argv[], char* env[])
+{
+
+    cout << "ZMIENNE ŚRODOWISKOWE : \n";
+    system("set");
+    cout << "PARAMETRY PROGRAMU : \n";
+    for (int i = 0; i < argc; i++) {
+        cout << argv[i] << endl;
+    }
+}
