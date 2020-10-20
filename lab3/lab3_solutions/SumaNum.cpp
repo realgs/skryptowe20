34969af78e6a1b39e38eec2bed843c06068804c6
kwarc-agat <aga.kwarc@gmail.com>
2020-10-20 13:25:27 +0200

    add SumaNum

---
+++ lab3/lab3_solutions/SumaNum.cpp
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+#define MAX_INPUT_LENGTH 500
+
+int readStdInToCharArray(char** text)
+{
+    int textSize = 0;
+
+    while (!cin.eof()) {
+        cin >> noskipws >> (*text)[textSize];
+        textSize++;
+    }
+    return textSize;
+}
+
+int main(int argc, char** argv, char** env)
+{
+	char* text = new char[MAX_INPUT_LENGTH];
+    int text_size = readStdInToCharArray(&text);
+    char* input_string = strtok(text, " \n\t");
+    double sum = 0;
+    while (input_string != NULL)
+    {
+        cout << input_string <<endl;
+        double number = atof(input_string);
+        if (number != 0)
+            sum += number;
+        input_string = strtok(NULL, " \n\t");
+    }
+    cout << "\nSum: " << sum;
+}
