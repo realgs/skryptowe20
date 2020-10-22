28b9ef07b59d0c3e19e6fd4729353993a6341e96
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-22 19:05:28 +0200

    Changed opening file to cin

--- StarczyJeden.cpp
+++ StarczyJeden.cpp
@@ -3,12 +3,9 @@
 #include <vector>
 
 int main(int argc, char *argv[]){
-    std::ifstream infile(
-            "/Users/piotrrasinski/OneDrive - Politechnika Wroclawska/Semestr5/Skryptowe_L/skryptowe20/zakupy.txt");
-
     std::string input_line;
     std::vector<std::string> output_rows;
-    while (std::getline(infile, input_line)) {
+    while (std::getline(std::cin, input_line)) {
         for (int i = 1; i < argc; i++) {
             if (input_line.find(argv[i]) != std::string::npos)
                 output_rows.push_back(input_line);
