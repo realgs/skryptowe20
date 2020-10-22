90fecf17d390d3dd48a9c900c3091abd0e640660
DamianZ <dzoltow@gmail.com>
2020-10-22 02:47:10 +0200

    added solutions to task 1 and task 2

---
+++ lab3/SelKol/SelKol/SelKol.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+#define DELIMITER_OF_COLUMNS "\t"
+#define NO_ARGUMENTS_GIVEN 11
+#define WRONG_COLUMN_INDEX 12
+#define NO_DELIMITER_DETECTED 13
+
+int main(int argc, char* argv[], char* env[])
+{   
+    std::cout << "Podaj tekst to slekecji kolumn (wyrazy oddzielone oddzielone tabluacją:" << std::endl;
+    std::string userInput;
+    std::getline(std::cin, userInput);
+    std::vector <std::string> textToBeDisplayed;
+
+    while (userInput != "" && userInput.find(DELIMITER_OF_COLUMNS) != std::string::npos) {
+        size_t separatorIndex = userInput.find(DELIMITER_OF_COLUMNS);
+        textToBeDisplayed.push_back(userInput.substr(0, separatorIndex));
+        userInput = userInput.substr(separatorIndex + 1, userInput.length());
+    }
+    textToBeDisplayed.push_back(userInput);
+    if (argc < 2) { return NO_ARGUMENTS_GIVEN; }
+    for (int i = 1; i < argc; i++) {
+        int index = strtol(argv[i], nullptr , 10) - 1;
+        if (index < textToBeDisplayed.size()) {
+            std::cout << textToBeDisplayed.at(index) << DELIMITER_OF_COLUMNS;
+        } 
+        else {
+            return WRONG_COLUMN_INDEX;
+        }
+    }
+}
---
+++ lab3/SumaNum/SumaNum/SumaNum.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define NO_PARAMETERS_GIVEN 11;
+
+bool isNumber(char number[])
+{
+    int i = 0;
+
+    if (number[0] == '-')
+        i = 1;
+    for (; number[i] != '\0'; i++)
+    {
+        if (!isdigit(number[i]))
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[], char* env[])
+{
+	int sum = 0;
+	if (argc < 2) { return NO_PARAMETERS_GIVEN; }
+	
+	for (int i = 1; i < argc; i++)
+	{
+        if (isNumber(argv[i])) {
+            sum += strtol(argv[i], NULL, 10);
+        }
+	}
+	std::cout << "The sum is: " << sum;
+}
