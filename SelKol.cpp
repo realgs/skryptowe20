616e1106b33ce758ecb52b1e334b3fb79ca2ba66
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-22 16:25:14 +0200

    Implemented main loop and added executable

--- SelKol.cpp
+++ SelKol.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-
     if (argc > 4)
         return 1;
     for (int i = 0; i < argc; i++) {
@@ -9,5 +12,21 @@ int main(int argc, char *argv[]) {
             return 2;
     }
 
+    std::ifstream infile(
+            "/Users/piotrrasinski/OneDrive - Politechnika Wroclawska/Semestr5/Skryptowe_L/skryptowe20/zakupy.txt");
+    std::string line;
+    std::vector<std::string> output;
+
+    while (std::getline(infile, line)) {
+        std::istringstream iss(line);
+        std::string word;
+        while (iss >> word)
+            output.push_back(word);
+
+        for (int i = 1; i < argc; i++)
+            std::cout << output[atoi(argv[i]) - 1] << '\t';
+
+        std::cout << std::endl;
+    }
     return 0;
 }
