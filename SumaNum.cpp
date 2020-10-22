dc5a1fc332541e451f8a94549de6d3247404d531
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-22 19:03:50 +0200

    Changed main loop and sum counting

--- SumaNum.cpp
+++ SumaNum.cpp
@@ -2,22 +2,12 @@
 #include <fstream>
 
 int main(int argc, char *argv[]) {
-    std::ifstream infile(
-            "/Users/piotrrasinski/OneDrive - Politechnika Wroclawska/Semestr5/Skryptowe_L/skryptowe20/zakupy.txt");
-
-    int counter = 0;
     double sum = 0.0;
     std::string word;
-    while (infile >> word) {
-        if (counter == 2) {
-            sum += std::stod(word);
-            counter++;
-        } else if (counter == 3) {
-            sum += std::stod(word);
-            counter = 0;
-        } else {
-            counter++;
-        }
+    while (std::cin >> word) {
+        try {
+            sum += stod(word);
+        } catch (const std::invalid_argument& e) {}
     }
     std::cout << sum << std::endl;
     return 0;
