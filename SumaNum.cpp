581630b5e6ff6378049aff59f1c2a51195ef6465
Dzejcek <jac.mikaa@gmail.com>
2020-10-23 20:34:30 +0200

    add task 2

---
+++ SumaNum.cpp
@@ -0,0 +1,22 @@
+﻿// Skryptowe_lab3.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <vector>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    double amount = 0;
+    string number;
+    while (cin >> number)
+    {
+        amount += stod(number);
+    }
+    cout << amount << endl;
+    return 0;
+    
+}
+
