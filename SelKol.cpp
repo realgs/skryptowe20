9d0bcb00a58e4a5cbbfe704dec3f51b84b2b4587
ShakiMan <shakiman666@gmai.com>
2020-10-23 21:51:16 +0200

    First task, text file

---
+++ SelKol.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+
+    if (argc > 5) return 11;
+    for (int i = 1; i < argc; i++)
+    {
+        if (atoi(argv[i]) > 4) return 12;
+    }
+
+    vector<int> selCol;
+    for (int i = 1; i < argc; i++)
+    {
+        selCol.push_back(atoi(argv[i]));
+    }
+
+    ifstream source;
+    source.open("Zakup.txt");
+    vector<string> lines;
+    string line;
+    while (getline(source, line))
+    {
+        stringstream ss(line);
+        string token;
+        vector<string> tokens;
+        while (getline(ss, token, '\t')) {
+            tokens.push_back(token);
+        }
+        for (int i = 0; i < static_cast<int>(selCol.size()); i++)
+        {
+            int k = selCol[i];
+            cout << tokens[k-1] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
