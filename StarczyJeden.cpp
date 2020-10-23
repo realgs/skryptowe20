1364c45c8a24372bfe3b3c143ec7bdf6e56b4f90
ShakiMan <shakiman666@gmai.com>
2020-10-24 01:57:10 +0200

    Third task

---
+++ StarczyJeden.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	string line;
+	bool include = false;
+
+	while (getline(cin, line))
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			if (line.find(argv[i]) != string::npos)	include = true;
+		}
+		
+		if (include)
+		{
+			cout << line << endl;
+			include = false;
+		}
+	}
+
+	return 0;
+}
