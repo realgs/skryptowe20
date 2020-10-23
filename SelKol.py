a6677d4608e0a1a4c8153cb075f8aeb4d467f6fe
Pawel <mrprzyprawa@gmail.com>
2020-10-23 13:57:54 +0200

    Add SelKol

---
+++ SelKol.py
@@ -0,0 +1,33 @@
+import sys
+
+SEP = '\t'
+
+def select_columns(argv):
+  try:
+    columns = [int(x) for x in argv[1:]]
+    for x in columns:
+      if x < 1 or x > 4:
+        print("Wrong argument")
+        exit(1)
+    return columns
+  except Exception:
+    print("Wrong arguments")
+    exit(1)
+
+def print_columns(columns):
+  while True:
+    try:
+      line = input()
+      if line == '':
+        break
+
+      product_col = line.split(SEP)
+      to_print = ''
+      for col in columns:
+        to_print = to_print + product_col[col - 1] + SEP
+      print(to_print)
+    except EOFError:
+      break
+
+columns = select_columns(sys.argv)
+print_columns(columns)
