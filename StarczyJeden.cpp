1c6244fb917c4d8e15c418e8394a3a8796ad7c8f
Dzejcek <jac.mikaa@gmail.com>
2020-10-23 20:47:20 +0200

    add task 3

---
+++ StarczyJeden.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <regex>
+#include <vector>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    fstream file;
+    string word;
+    int numberOfColumns = 4;
+
+    vector<regex> phrase;
+    string line;
+    for (int i = 1; i < argc; i++) {
+        phrase.push_back(regex(argv[i]));
+    }
+    while (getline(cin, line)) {
+        for (int i = 0; i < phrase.size(); i++) {
+            if (regex_search(line, phrase[i])) {
+                cout << line << endl;
+                break;
+            }
+
+        }
+
+    }
+}
