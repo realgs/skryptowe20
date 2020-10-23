9d8188e1d9c2fc08df019866b8a0402705184f3b
ShakiMan <shakiman666@gmai.com>
2020-10-24 00:54:37 +0200

    Second task

---
+++ SumaNum.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+    string foo;
+    double sum = 0.0;
+    double value;
+    while (cin >> foo)
+    {
+        try
+        {
+            value = stod(foo);
+            sum += value;
+
+        }
+        catch (const std::exception& e)
+        {
+
+        }
+    }
+    
+    cout << "Sum:" << sum << endl;
+    return 0;
+}
