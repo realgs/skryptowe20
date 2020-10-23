5862163c1a4d4a23d90854f15873e5b6d63712b8
Kamil <kogol99@gmail.com>
2020-10-24 01:42:09 +0200

    add StarczyJeden

---
+++ lab3/StarczyJeden.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string textLine;
+    while (cin >> textLine)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            size_t found = textLine.find(argv[i]);
+            if (found != string::npos)
+            {
+                cout << textLine;
+                break;
+            }
+        }
+    }
+}
