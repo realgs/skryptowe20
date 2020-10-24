89f66679a2b8932fe6c4928d2bc70275a8214d40
joannasroka <joannasroka98@gmail.com>
2020-10-24 22:08:58 +0200

    added StarczyJeden

---
+++ lab3_solutions/StarczyJeden.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void readAndPrintMatchingLines(int argc, char* argv[]) {
+	string input;
+	string inputToLower;
+	string paramToLower;
+
+	bool found = false;
+
+	while (getline(cin, input)) {
+		for (int i = 1; i < argc; i++) {
+			inputToLower = input;
+			transform(inputToLower.begin(), inputToLower.end(), inputToLower.begin(), ::tolower);
+
+			paramToLower = argv[i];
+			transform(paramToLower.begin(), paramToLower.end(), paramToLower.begin(), ::tolower);
+
+			if (inputToLower.find(paramToLower) != string::npos) {
+				found = true;
+			}
+		}
+		if (found) cout << input << endl;
+		found = false;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	readAndPrintMatchingLines(argc, argv);
+}
