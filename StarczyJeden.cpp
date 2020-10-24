c07ba7ebc1f952b112eb5703da5196ea39714dca
Konrad <konrad.hajduga@gmail.com>
2020-10-24 18:36:43 +0200

    add third task

---
+++ StarczyJeden.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    std::string userInput;
+
+    while (getline(std::cin, userInput)) {
+        bool containsParameter = false;
+        for (int i = 0; i < argc && !containsParameter; i++) {
+            containsParameter = userInput.find(argv[i]) != std::string::npos;
+            if (containsParameter) {
+                std::cout << userInput << "\n";
+            }
+        }
+    }
+}
