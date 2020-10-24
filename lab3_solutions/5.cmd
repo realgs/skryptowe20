576214133c28c4c00d80845e165a3602ab510df3
joannasroka <joannasroka98@gmail.com>
2020-10-24 22:38:52 +0200

    changed 5 - added hardcoded version

--- lab3_solutions/5.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-doskey macro_4a=4a.cmd truskawki
-doskey macro_4b=4b.cmd ZIEMNIAKI TRUSKAWKI
-doskey macro_4c=4c.cmd 01.10.2020
-doskey macro_4d=4d.cmd 01.10.2020
---
+++ lab3_solutions/5_hardcoded.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+doskey macro_4a=4a truskawki
+doskey macro_4b=4b ZIEMNIAKI TRUSKAWKI
+doskey macro_4c=4c 01.10.2020
+doskey macro_4d=4d 01.10.2020
