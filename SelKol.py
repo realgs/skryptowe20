d73f90521340c2ac1824d383ec927a2803644ca7
Maciej Wasilewski <wasilewski078@gmail.com>
2020-10-24 18:11:50 +0200

    Added SelKol.py

---
+++ SelKol.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+
+
+def interpret_stdin(stdinputs):
+    columns = {1: [], 2: [], 3: [], 4: []}
+    for i, line in enumerate(stdinputs):
+        data = line.split()
+        date, name, weight, price = data
+        columns[1].append(date)
+        columns[2].append(name)
+        columns[3].append(weight)
+        columns[4].append(price)
+    return columns
+
+
+def print_columns(columns_numbers, columns):
+    output = ""
+    for i in range(len(columns[1])):
+        for col_number in columns_numbers:
+            output += columns[int(col_number)][i] + "\t"
+        output += "\n"
+    print(output)
+
+
+if __name__ == "__main__":
+    columns = interpret_stdin([x.strip("\n") for x in sys.stdin])
+    print_columns(sys.argv[1:], columns)
+    
