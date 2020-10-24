efc8ea8570fa97938f129dfea3f116c72cb8026d
joannasroka <joannasroka98@gmail.com>
2020-10-24 22:08:38 +0200

    added SumaNum

---
+++ lab3_solutions/SelKol.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+#define COLUMNS_SEPARATOR '\t'
+
+void readColumns(vector<vector<string>>* separatedColumns, int* numberOfLines) {
+	string input;
+	string singleColumn;
+
+	while (getline(cin, input)) {
+		stringstream columns(input);
+		vector<string> line;
+
+		while (getline(columns, singleColumn, COLUMNS_SEPARATOR)) {
+			line.push_back(singleColumn);
+		}
+		separatedColumns->push_back(line);
+		(*numberOfLines)++;
+	}
+}
+
+void printSelectedColumns(vector<vector<string>>* separatedColumns, int numbersOfLines, char* argv[], int argc) {
+	int index;
+	for (int i = 0; i < numbersOfLines; i++) {
+		for (int j = 1; j < argc; j++) {
+			index = atoi(argv[j]);
+			if (index < (*separatedColumns)[i].size()) cout << (*separatedColumns)[i][index] << COLUMNS_SEPARATOR;
+		}
+		cout << endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	vector<vector<string>> separatedColumns;
+	int numbersOfLines = 0;
+	readColumns(&separatedColumns, &numbersOfLines);
+	printSelectedColumns(&separatedColumns, numbersOfLines, argv, argc);
+}
---
+++ lab3_solutions/SumaNum.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+double calculateSum() {
+	string number;
+	double sum = 0.0;
+
+	while (!cin.eof()) {
+		number = "";
+		cin >> number;
+
+		try {
+			sum += stod(number);
+		}
+		catch (invalid_argument exc) {};
+	}
+	return sum;
+}
+
+int main()
+{
+	cout << calculateSum();
+}
