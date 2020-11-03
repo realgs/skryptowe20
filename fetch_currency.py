879a4c9ff75efcd183da3c26f765bfed8c606e5f
Jedrzej Rochala <rochala@tutanota.com>
2020-11-03 16:30:05 +0100

    add sql methods

--- fetch_currency.py
+++ fetch_currency.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import requests
+import sqlite3
 from enum import Enum
 from datetime import datetime, timedelta
+
+LIMIT = 366
 
 
 class Currency(Enum):
@@ -12,13 +15,24 @@ class Currency(Enum):
 
 
 def get_previous_days(currency, days):
-    if currency not in Currency._value2member_map_ or days < 1:
-        print('Invalid arguments, please try again')
-    else:
+    if currency not in Currency._value2member_map_ or not 0 < days < 367:
         current_date = datetime.now()
         beginning = current_date - timedelta(days=days)
         response = requests.get(
             f'http://api.nbp.pl/api/exchangerates/rates/a/{currency}/{beginning.date()}/{current_date.date()}/?format=json')
+        if(response.status_code != 200):
+            print(f'Request Error {response.status_code}')
+            return response.status_code
+        else:
+            return response
+
+
+def get_range(currency, start_date, end_date):
+    if currency not in Currency._value2member_map_ or not 0 < (end_date - start_date).days < 367:
+        print('Invalid arguments, please try again')
+    else:
+        response = requests.get(
+            f'http://api.nbp.pl/api/exchangerates/rates/a/{currency}/{start_date.date()}/{end_date.date()}/?format=json')
         if(response.status_code != 200):
             print(f'Request Error {response.status_code}')
             return response.status_code
@@ -32,7 +46,7 @@ def parse_response(response):
     print(response.json()['rates'][1])
     for value in response.json()['rates']:
         y.append(value['mid'])
-        x.append(datetime.strptime(value['effectiveDate'], '%Y-%m-%d'))
+        x.append(to_date(value['effectiveDate']))
     return x, y
 
 
@@ -51,7 +65,6 @@ def create_chart(currencies, days):
                 print(f'Failed fetching {currency}')
             else:
                 x, y = parse_response(response)
-                print(y)
                 ax.plot(x, y, label=currency)
 
         ax.xaxis_date()
@@ -63,4 +76,65 @@ def create_chart(currencies, days):
         plt.show()
 
 
-create_chart(['USD', 'EUR'], 183)
+def fix_response(response):
+    x = []
+    y = []
+    previous_date = response.json()['rates'][0]
+    for value in response.json()['rates']:
+        for date in range(1, (to_date(value['effectiveDate']) - to_date(previous_date['effectiveDate'])).days):
+            y.append(previous_date['mid'])
+            x.append(to_date(previous_date['effectiveDate']) + timedelta(days=date))
+        y.append(value['mid'])
+        x.append(to_date(value['effectiveDate']))
+        previous_date = value
+
+    return x, y
+
+
+def to_date(str_date):
+    return datetime.strptime(str_date, '%Y-%m-%d')
+
+
+def fill_currency(currency, start_date, end_date):
+    dates = split_date(start_date, end_date)
+    x = []
+    y = []
+    for date_range in dates:
+        response = get_range(currency, date_range[0], date_range[1])
+        if isinstance(response, int) or response is None:
+            print(f'Failed fetching data between {date_range[0]} and {date_range[1]}')
+        else:
+            fixed_data = fix_response(response)
+            x.append(fixed_data[0])
+            y.append(fixed_data[1])
+
+    conn = sqlite3.connect('sales.db')
+    c = conn.cursor()
+    print("Opened database successfully")
+    x = [item for lst in x for item in lst]
+    y = [item for lst in y for item in lst]
+    print(x[0])
+    for date, value in zip(x, y):
+        print(date, value)
+        safe_data = (datetime.strftime(date, '%Y-%m-%d'), value,)
+        c.execute('INSERT INTO currency VALUES(?,?)', safe_data)
+
+    conn.commit()
+    conn.close()
+
+
+def split_date(start_date, end_date):
+    days_between = (end_date - start_date).days
+    print(days_between)
+    dates = []
+    while days_between != 0:
+        if days_between >= LIMIT:
+            dates.append((end_date - timedelta(days=days_between), end_date -
+                          timedelta(days=days_between) + timedelta(days=LIMIT - 1)))
+            days_between -= LIMIT
+        else:
+            dates.append((end_date - timedelta(days=days_between), end_date))
+            days_between = 0
+    return dates
+
+    # create_chart(['USD', 'EUR'], 183)
---
+++ sql
@@ -0,0 +1,3 @@
+"Stworzenie tabeli z wartosciami waluty wzgledem dolara
+CREATE TABLE currency ("date" date NOT NULL PRIMARY KEY, "value" REAL NOT NULL);
+"Dodanie wartosci poprzez kod w pythonie
