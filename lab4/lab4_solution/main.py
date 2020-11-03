901244723bef32be97c8b85ef9adf25005069832
kwarc-agat <aga.kwarc@gmail.com>
2020-11-03 21:04:10 +0100

    add ex.2

--- lab4/lab4_solution/main.py
+++ lab4/lab4_solution/main.py
@@ -2,5 +2,6 @@ import nbp
 
 def main():
     print(nbp.get_avg_ex_rate("EUR", 5))
+    dollars, euros = nbp.get_dollar_euro_half_year()
 if __name__ == "__main__":
     main()
--- lab4/lab4_solution/nbp.py
+++ lab4/lab4_solution/nbp.py
@@ -2,6 +2,9 @@ import requests
 from datetime import date, timedelta
 
 TODAY_DATE = date.today()
+HALF_YEAR_DAYS = 20
+DOLLAR = "USD"
+EURO = "EUR"
 
 def get_avg_ex_rate(currency, days_number):
     records_number = 0
@@ -16,5 +19,28 @@ def get_avg_ex_rate(currency, days_number):
             records_number += 1
         current_date = current_date - timedelta(days=1)
     return result
+
+def get_dollar_euro_half_year():
+    dollars = {}
+    euros = {}
+    start_date = (TODAY_DATE - timedelta(days=HALF_YEAR_DAYS)).strftime("%Y-%m-%d")
+    end_date = TODAY_DATE.strftime("%Y-%m-%d")
+
+    resp_dollars = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/{}/{}/{}/'.format(DOLLAR, start_date, end_date))
+    resp_euros = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/{}/{}/{}/'.format(EURO, start_date, end_date))
+
+    if resp_dollars.status_code != 200:
+            pass
+    else:
+        for record in resp_dollars.json()['rates']:
+            dollars[str(record['effectiveDate'])] = record['mid']   
+
+    if resp_euros.status_code != 200:
+            pass
+    else:
+        for record in resp_euros.json()['rates']:
+            euros[str(record['effectiveDate'])] = record['mid']
+    
+    return dollars, euros
 
     
