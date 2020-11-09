83c55e28dd988942288403558e46b80990d77779
Mateusz Groblicki <lokigameplayer@gmail.com>
2020-11-09 16:14:57 +0100

    added new functionalities
     last_rates and rates_time_range now accept new optional argument
     and now can return all range of rates including days without ratings
    
     regenerated total_sales_graph

--- lab4-api/nbp.py
+++ lab4-api/nbp.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+from datetime import timedelta
 import json
 import requests
 import datetime as dt
@@ -43,21 +44,16 @@ class Currency(Enum):
 
 def request_data(api_url: str) -> Optional[Any]:
     json_data = None
-    try:
-        response = requests.get(api_url, params={"?format": "json"})
-        if response:
-            json_data = response.json()
-    except requests.RequestException as err:
-        print(f"Failed to process request ERROR: {err}")
-    except json.JSONDecodeError as err:
-        print(f"Failed to decode json ERROR: {err}")
-    except Exception as err:
-        print(f"unexpected error ERROR: {err}")
+
+    response = requests.get(api_url, params={"?format": "json"})
+    if response:
+        json_data = response.json()
 
     return json_data
 
 
-def rates_time_range(currency: Currency, start_date: dt.date, end_date: dt.date) -> List[Tuple[float, dt.date]]:
+def rates_time_range(currency: Currency, start_date: dt.date, end_date: dt.date,
+                     include_unrated_days: bool = False) -> List[Tuple[float, dt.date]]:
     total_days = (end_date - start_date).days
     if total_days < 0:
         raise ValueError("dates are in wrong order")
@@ -66,18 +62,21 @@ def rates_time_range(currency: Currency, start_date: dt.date, end_date: dt.date)
 
     # split ranges into chunks to meet API requirements
     chunks: List[Tuple[str, str]] = []
-    while total_days >= 0:
-        if total_days == 0:
+    tmp_start_date = start_date
+    tmp_end_date = end_date
+    tmp_total_days = total_days
+    while tmp_total_days >= 0:
+        if tmp_total_days == 0:
             chunks.append((start_date.strftime(DATE_FORMAT),
                            start_date.strftime(DATE_FORMAT)))
             break
 
-        days = min(total_days, API_DAYS_LIMIT)
-        end_date = start_date + dt.timedelta(days=days)
-        chunks.append((start_date.strftime(DATE_FORMAT),
-                       end_date.strftime(DATE_FORMAT)))
-        total_days -= days + 1
-        start_date = end_date + dt.timedelta(days=1)
+        days = min(tmp_total_days, API_DAYS_LIMIT)
+        tmp_end_date = tmp_start_date + dt.timedelta(days=days)
+        chunks.append((tmp_start_date.strftime(DATE_FORMAT),
+                       tmp_end_date.strftime(DATE_FORMAT)))
+        tmp_total_days -= days + 1
+        tmp_start_date = tmp_end_date + dt.timedelta(days=1)
 
     for chunk in chunks:
         api_response = request_data(
@@ -88,19 +87,61 @@ def rates_time_range(currency: Currency, start_date: dt.date, end_date: dt.date)
                 data.append(
                     (rate[ApiFields.MID], dt.datetime.strptime(rate[ApiFields.EFFECTIVE_DATE], DATE_FORMAT).date()))
 
+    ###### fill gaps in rates ######
+    if include_unrated_days:
+        tmp_rates: List[Tuple[float, dt.date]] = []
+        day_offset = 1
+        missing_rates = None
+        while not data or (data and data[0][1] > start_date):
+            missing_rates = rates_time_range(
+                currency, start_date - timedelta(days=day_offset), start_date)
+            if missing_rates:
+                data = missing_rates + data
+            day_offset += 1
+
+        # add trailing rates if missing
+        if data[-1][1] < end_date:
+            tmp_date = data[-1][1]
+            for _ in range((end_date - data[-1][1]).days):
+                tmp_date = tmp_date + timedelta(days=1)
+                data.append((data[-1][0], tmp_date))
+
+        # add the rest of missing rates
+        if (data[-1][1] - data[0][1]).days > len(data):
+            for index, rate in enumerate(data[:-1]):
+                days_difference = (data[index + 1][1] - rate[1]).days
+                if days_difference > 1:
+                    tmp_date = rate[1]
+                    for _ in range(days_difference - 1):
+                        tmp_date = tmp_date + timedelta(days=1)
+                        tmp_rates.append((rate[0], tmp_date))
+            data.extend(tmp_rates)
+            data.sort(key=lambda item: item[1])
+
+        data = data[-(total_days + 1):]
+
     return data
 
 
-def last_rates(currency: Currency, days: int = 1) -> List[Tuple[float, dt.date]]:
+def last_rates(currency: Currency, days: int = 1,
+               include_unrated_days: bool = False) -> List[Tuple[float, dt.date]]:
     '''
     1. Stworzyć funkcję pobierającą średnie kursy notowań zadanej parametrem waluty z ostatnich X dni.
     '''
     if days < 1:
         raise ValueError("day count cannot be lower than one")
 
-    now = dt.datetime.now().date()
-    return rates_time_range(currency, now - dt.timedelta(days=days - 1), now)
+    now = dt.datetime.now()
+    return rates_time_range(currency, now - dt.timedelta(days=days - 1), now, include_unrated_days)
 
 
 if __name__ == "__main__":
-    print(last_rates(Currency.EUROPEAN_EURO, 5))
+    # tests
+    res = last_rates(Currency.EUROPEAN_EURO, 120, include_unrated_days=True)
+    print(len(res))
+    res = last_rates(Currency.EUROPEAN_EURO, 120, include_unrated_days=False)
+    print(len(res))
+    print(rates_time_range(Currency.EUROPEAN_EURO, dt.date(2020, 11, 7),
+                           dt.date(2020, 11, 8), include_unrated_days=False))
+    print(rates_time_range(Currency.EUROPEAN_EURO, dt.date(2020, 11, 7),
+                           dt.date(2020, 11, 8), include_unrated_days=True))
