104a12d21ae2ece60ef2f2e8e933275f9cacf888
DamianBartosz <damian.mateusz.bartosz@gmail.com>
2020-11-14 23:55:23 +0100

    Define getAverageExchangeRatesInDays

--- lab4/ApiService.py
+++ lab4/ApiService.py
@@ -1,8 +1,28 @@
 import requests
+from datetime import date, timedelta
 
 from Exceptions.ApiError import ApiError
 
 
 def getAverageExchangeRatesInDays(currencyCode, days):
-    pass
+    startDate = date.today() - timedelta(days=days)
+
+    url = "http://api.nbp.pl/api/exchangerates/rates/"
+    paramsUrl = "/{}/{}/{}".format(currencyCode, startDate, date.today())
+    resp = requests.get(url + "a" + paramsUrl)
+
+    if resp.status_code != 200:
+        if resp.status_code == 400:
+            raise ApiError("Invalid days param")
+
+        resp = requests.get(url + "b" + paramsUrl)
+        if resp.status_code != 200:
+            raise ApiError("No data found")
+
+    returnData = []
+    for item in resp.json()["rates"]:
+        returnData.append(item)
+
+    return returnData
+
 
