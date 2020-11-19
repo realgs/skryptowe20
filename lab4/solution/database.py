2324f635000455096dea2e3bc2d4571c3cb7349d
Tomasz Drozdz <t.p.drozdz@gmail.com>
2020-11-19 21:58:30 +0100

    Changed searching algorithm, split functions.

--- lab4/solution/database.py
+++ lab4/solution/database.py
@@ -1,5 +1,5 @@
 from os.path import isfile
-from datetime import date, timedelta, datetime
+from datetime import date, datetime
 import sqlite3
 
 import pandas as pd
@@ -9,11 +9,12 @@ import matplotlib.dates as dt
 from rates import daily_exchange_rates_for, Currency, DATE_FORMAT
 
 
-# Pelna wersja AdventureWorks
-# Zawiera sprzedaz tylko od 2011 do 2014, robie wykres dla calego tego okresu.
-# Srednie notowania dla USD od 2010
-AW_DATABSE_FILE = "./AdventureWorks2019.sqlite"
-DATABASE_DATE_FORMAT = "%Y-%m-%d"
+# Dane dla okresu: 2016-01-01 - 2018-01-01
+# SQL Server Sample Database Bike Stores przekonwertowane na SQLite
+
+DB_FILE = "./BikeStores.db"
+DB_CUSTOM_DATE_FORMAT = "%Y-%m-%d"
+DB_READ_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 def connect_to_existing_file(file_):
@@ -23,7 +24,12 @@ def connect_to_existing_file(file_):
     return sqlite3.connect(file_)
 
 
-def create_currency_table(connection, currency, days_num):
+def create_currency_table(connection, currency, from_date, to_date):
+    if from_date > to_date:
+        raise ValueError(
+            f"from_date '{from_date}' should be before to_date '{to_date}'"
+        )
+
     cursor = connection.cursor()
     cursor.execute(
         """
@@ -34,21 +40,19 @@ def create_currency_table(connection, currency, days_num):
         """
     )
 
+    days_num = (date.today() - from_date).days
     usd = daily_exchange_rates_for(currency, days_num)
     df = pd.DataFrame(usd)
     df.set_index("date", inplace=True)
 
-    today = date.today()
-    expected_start = today - timedelta(days=days_num - 1)
-
-    new_index = pd.date_range(expected_start, today)
+    new_index = pd.date_range(from_date, to_date)
     df = df.reindex(index=new_index, method="pad")
 
     # Fill the remaining None values by looking for the first valid value,
     # in case the data starts on a weekend or holiday (the number of days might vary).
     df.fillna(method="backfill", inplace=True)
 
-    df.index = df.index.strftime(DATABASE_DATE_FORMAT)
+    df.index = df.index.strftime(DB_CUSTOM_DATE_FORMAT)
 
     cursor.executemany(
         """
@@ -61,25 +65,48 @@ def create_currency_table(connection, currency, days_num):
     connection.commit()
 
 
-def chart_sales_in_two_currencies(connection, days_num):
+def get_sales_in_two_currencies(connection, from_date, to_date):
+    if from_date > to_date:
+        raise ValueError(
+            f"from_date '{from_date}' should be before to_date '{to_date}'"
+        )
+
     cursor = connection.cursor()
 
     cursor.execute(
         """
-        SELECT orderdate, SUM(totaldue) FROM salesorderheader
-        GROUP BY orderdate
-        ORDER BY orderdate
-        """
+        SELECT order_date, ROUND(SUM(list_price * (1 - discount) * quantity), 2)
+        FROM orders O JOIN order_items OI ON O.order_id = OI.order_id
+        GROUP BY order_date
+        HAVING order_date BETWEEN ? AND ?
+        ORDER BY order_date
+        """,
+        (
+            from_date.strftime(DB_READ_DATE_FORMAT),
+            to_date.strftime(DB_READ_DATE_FORMAT),
+        ),
     )
 
     original_sales = [
-        (datetime.strptime(date.split("T")[0], DATABASE_DATE_FORMAT), total)
+        (datetime.strptime(date.split(" ")[0], DB_CUSTOM_DATE_FORMAT), total)
         for date, total in cursor
     ]
 
-    rates = cursor.execute("SELECT date, rate FROM pln_rates ORDER BY date")
+    rates = cursor.execute(
+        """
+        SELECT date, rate
+        FROM pln_rates
+        WHERE date BETWEEN ? AND ?
+        ORDER BY date
+        """,
+        (
+            from_date.strftime(DB_CUSTOM_DATE_FORMAT),
+            to_date.strftime(DB_CUSTOM_DATE_FORMAT),
+        ),
+    )
+
     rates = [
-        (datetime.strptime(date, DATABASE_DATE_FORMAT), rate) for date, rate in cursor
+        (datetime.strptime(date, DB_CUSTOM_DATE_FORMAT), rate) for date, rate in cursor
     ]
 
     sales_headers = ["date", "total"]
@@ -94,17 +121,22 @@ def chart_sales_in_two_currencies(connection, days_num):
         [sales["date"], exchanged_total_column], axis=1, keys=sales_headers
     )
 
+    return sales, exchanged_sales
+
+
+def draw_chart_for(sales, exchanged_sales):
+    plt.figure(figsize=(10, 5))
     plt.gca().xaxis.set_major_formatter(dt.DateFormatter(DATE_FORMAT))
 
     plt.plot(
         exchanged_sales["date"],
         exchanged_sales["total"],
         label="Sales [PLN]",
-        alpha=0.7,
-        linewidth=0.8,
+        linewidth=0.9,
+        alpha=0.9,
     )
     plt.plot(
-        sales["date"], sales["total"], label="Sales [USD]", alpha=0.7, linewidth=0.8
+        sales["date"], sales["total"], label="Sales [USD]", linewidth=0.9, alpha=0.9
     )
 
     plt.gcf().autofmt_xdate()
@@ -121,10 +153,13 @@ def chart_sales_in_two_currencies(connection, days_num):
 
 
 if __name__ == "__main__":
-    connection = connect_to_existing_file(AW_DATABSE_FILE)
+    connection = connect_to_existing_file(DB_FILE)
+    from_date = date(2016, 1, 1)
+    to_date = date(2018, 1, 1)
 
-    days_num = (date.today() - date(2010, 1, 1)).days
-    create_currency_table(connection, Currency.USD, days_num)
-    chart_sales_in_two_currencies(connection, days_num)
+    days_num = (date.today() - from_date).days
+    create_currency_table(connection, Currency.USD, from_date, to_date)
+    sales, exchanged_sales = get_sales_in_two_currencies(connection, from_date, to_date)
+    draw_chart_for(sales, exchanged_sales)
 
     connection.close()
