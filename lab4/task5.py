09845b1d3867c8d8ed38c7a2c41f9ad96dc14ed8
DamianZ <dzoltow@gmail.com>
2020-11-19 17:49:23 +0100

    "code fixes"

--- lab4/task5.py
+++ lab4/task5.py
@@ -18,7 +18,7 @@ def drawSalesChart():
                       ORDER BY date''')
     for date in cursor.fetchall():
         dates.append(datetime.datetime.strptime(date[0], '%Y-%m-%d').date())
-    cursor.execute('''SELECT order_date, SUM(sales), SUM(price)
+    cursor.execute('''SELECT order_date, SUM(sales), price
                       FROM SalesOrder JOIN CurrencyQuotes ON order_date = date
                       WHERE order_date BETWEEN '2013-01-01' AND '2015-12-31'
                       GROUP BY order_date
@@ -27,8 +27,8 @@ def drawSalesChart():
         USDSales.append(data[1])
         PLNSales.append(data[2] * data[1])
 
-    plt.plot(dates, USDSales, color='red', label='USD SALES', marker='^', linestyle='-')
-    plt.plot(dates, PLNSales, color='blue', label='PLN SALES', marker ='.', linestyle='-')
+    plt.plot(dates, USDSales, color='red', label='USD SALES', linestyle='-')
+    plt.plot(dates, PLNSales, color='blue', label='PLN SALES',  linestyle='-')
     plt.xlabel('Date', fontsize=14)
     plt.ylabel('Sales Value', fontsize=14)
     plt.legend()
