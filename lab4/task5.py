357655c1a1b38b92545564598cd479778737da07
DamianZ <dzoltow@gmail.com>
2020-11-19 17:47:03 +0100

    fixed chart to show proper values

--- lab4/task5.py
+++ lab4/task5.py
@@ -13,25 +13,28 @@ def drawSalesChart():
     PLNSales = []
     cursor.execute('''SELECT date 
                       FROM CurrencyQuotes 
+                      WHERE date BETWEEN '2013-01-01' AND '2015-12-31'
                       GROUP BY date
                       ORDER BY date''')
     for date in cursor.fetchall():
         dates.append(datetime.datetime.strptime(date[0], '%Y-%m-%d').date())
     cursor.execute('''SELECT order_date, SUM(sales), SUM(price)
                       FROM SalesOrder JOIN CurrencyQuotes ON order_date = date
+                      WHERE order_date BETWEEN '2013-01-01' AND '2015-12-31'
                       GROUP BY order_date
                       ORDER BY 1''')
-    for date in cursor.fetchall():
-        USDSales.append(date[1])
-        PLNSales.append(date[2])
+    for data in cursor.fetchall():
+        USDSales.append(data[1])
+        PLNSales.append(data[2] * data[1])
 
-    plt.plot(dates, USDSales, color='red', label='USD SALES')
-    plt.plot(dates, PLNSales, color='blue', label='PLN SALES')
+    plt.plot(dates, USDSales, color='red', label='USD SALES', marker='^', linestyle='-')
+    plt.plot(dates, PLNSales, color='blue', label='PLN SALES', marker ='.', linestyle='-')
     plt.xlabel('Date', fontsize=14)
     plt.ylabel('Sales Value', fontsize=14)
     plt.legend()
-    plt.title("PLN and USD total sales values during years 2013-2016")
+    plt.title("PLN and USD total sales values during years 2013-2015")
     plt.savefig('task5.svg')
     plt.show()
     connection.close()
 
+drawSalesChart()
