bd555fd415ec5bf42ce4db8125d2df52147b19d5
DamianZ <dzoltow@gmail.com>
2020-11-19 18:15:27 +0100

    "code fixes"

--- lab4/task5.py
+++ lab4/task5.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import sqlite3
 import datetime
 
@@ -13,28 +14,33 @@ def drawSalesChart():
     PLNSales = []
     cursor.execute('''SELECT date 
                       FROM CurrencyQuotes 
-                      WHERE date BETWEEN '2013-01-01' AND '2015-12-31'
                       GROUP BY date
                       ORDER BY date''')
     for date in cursor.fetchall():
-        dates.append(datetime.datetime.strptime(date[0], '%Y-%m-%d').date())
+        date = datetime.datetime.strptime(date[0], '%Y-%m-%d').strftime('%Y-%m')
+        dates.append(date)
     cursor.execute('''SELECT order_date, SUM(sales), price
-                      FROM SalesOrder JOIN CurrencyQuotes ON order_date = date
-                      WHERE order_date BETWEEN '2013-01-01' AND '2015-12-31'
+                      FROM SalesOrder JOIN CurrencyQuotes ON order_date = date                 
                       GROUP BY order_date
                       ORDER BY 1''')
     for data in cursor.fetchall():
         USDSales.append(data[1])
         PLNSales.append(data[2] * data[1])
 
-    plt.plot(dates, USDSales, color='red', label='USD SALES', linestyle='-')
-    plt.plot(dates, PLNSales, color='blue', label='PLN SALES',  linestyle='-')
+    plt.plot([], [], color='red', label='USD SALES', linewidth=3)
+    plt.plot([], [], color='blue', label='PLN SALES', linewidth=3)
+    ax = plt.axes()
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(7))
+    ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(20000))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(2000))
+    plt.stackplot(dates, USDSales, PLNSales, colors=['r', 'b'])
     plt.xlabel('Date', fontsize=14)
     plt.ylabel('Sales Value', fontsize=14)
     plt.legend()
-    plt.title("PLN and USD total sales values during years 2013-2015")
+    plt.title("PLN and USD total sales values during years 2013-2016")
     plt.savefig('task5.svg')
     plt.show()
     connection.close()
 
-drawSalesChart()
+
