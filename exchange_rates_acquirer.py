a47ccf60fc6c5b0536133cdd2181ddbab5fe865c
Norbert Nawrotek <norbertn15@gmail.com>
2020-11-20 04:58:50 +0100

    Add expanding exchange rates to every day from range

--- exchange_rates_acquirer.py
+++ exchange_rates_acquirer.py
@@ -3,6 +3,7 @@ import datetime
 
 MIN_AVAILABLE_DATE = "2002-01-02"
 MAX_DAYS = 93
+DELTA_ONE_DAY = datetime.timedelta(days=1)
 
 
 def format_datetime_to_string(date):
@@ -23,6 +24,29 @@ def make_get_request_for_range(currency_code, start_date, end_date):
         return result
     else:
         return []
+
+
+def make_get_request_for_single_day(currency_code, day_date):
+    day_date_string = format_datetime_to_string(day_date)
+
+    response = requests.get(
+        "http://api.nbp.pl/api/exchangerates/rates/a/{}/{}/?format=json".format(currency_code, day_date_string))
+    if response.status_code == 200:
+        rates_for_one_day = response.json()["rates"][0]
+        return rates_for_one_day["effectiveDate"], rates_for_one_day["mid"]
+    else:
+        return []
+
+
+def get_previous_available_day_date(currency_code, day_data):
+    day_data -= DELTA_ONE_DAY
+    single_day_data = make_get_request_for_single_day(currency_code, day_data)
+
+    while not single_day_data:
+        day_data -= DELTA_ONE_DAY
+        single_day_data = make_get_request_for_single_day(currency_code, day_data)
+
+    return single_day_data
 
 
 def split_dates_by_max_days(start_date, end_date):
@@ -50,10 +74,46 @@ def get_exchange_rates_from_api(currency_code, start_date, end_date):
     return exchange_rates
 
 
+def day_is_in_api(api_exchange_rates, date, api_rates_index):
+    if api_rates_index < len(api_exchange_rates):
+        if format_datetime_to_string(date) == api_exchange_rates[api_rates_index][0]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def expand_exchange_rates_to_range(api_exchange_rates, currency_code, start_date, end_date):
+    all_days_exchange_rates = []
+
+    if api_exchange_rates[0][0] != format_datetime_to_string(start_date):
+        previous_available_day_data = get_previous_available_day_date(currency_code, start_date)
+        # marks start_date with the value from previous available day
+        all_days_exchange_rates.append((format_datetime_to_string(start_date), previous_available_day_data[1]))
+        start_date += DELTA_ONE_DAY
+
+    api_rates_index = 0
+    while start_date <= end_date:
+        if day_is_in_api(api_exchange_rates, start_date, api_rates_index):
+            all_days_exchange_rates.append(api_exchange_rates[api_rates_index])
+            api_rates_index += 1
+        else:
+            all_days_exchange_rates.append(
+                (format_datetime_to_string(start_date), exchange_rates_from_api[api_rates_index - 1][1]))
+
+        start_date += DELTA_ONE_DAY
+
+    return all_days_exchange_rates
+
+
 if __name__ == "__main__":
     start = datetime.date(2020, 1, 1)
-    end = datetime.date(2020, 10, 10)
-    exchange_rates_from_api = get_exchange_rates_from_api("usd", start, end)
+    end = datetime.date(2020, 10, 12)
+    currency_usd = "usd"
+    exchange_rates_from_api = get_exchange_rates_from_api(currency_usd, start, end)
 
-    for rate in exchange_rates_from_api:
+    all_days_rates = expand_exchange_rates_to_range(exchange_rates_from_api, currency_usd, start, end)
+
+    for rate in all_days_rates:
         print(rate)
