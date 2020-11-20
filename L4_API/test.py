39931cebfe85df5cb168b7143fbd0ebeb2abd6fd
limisie <k.limisiewicz@gmail.com>
2020-11-20 15:50:36 +0100

    Add tests len(dates) == len(rates)

--- L4_API/test.py
+++ L4_API/test.py
@@ -1,43 +1,46 @@
 import unittest
-from .api_handler import currency_rates_dates, _get_table
+from .api_handler import currency_rates_and_dates, get_table
 
 
 class TestSortingMethods(unittest.TestCase):
 
     def test_table_getter(self):
-        table = _get_table('USD')
+        table = get_table('USD')
         self.assertEqual(table, 'A')
 
-        table = _get_table('JOD')
+        table = get_table('JOD')
         self.assertEqual(table, 'B')
 
-        table = _get_table('sdgdf')
+        table = get_table('sdgdf')
         self.assertEqual(table, '')
 
-        table = _get_table('')
+        table = get_table('')
         self.assertEqual(table, '')
 
     def test_currency_rates(self):
-        rates, _ = currency_rates_dates('USD', 1)
-        self.assertEqual(rates, [3.7872])
+        rates, _ = currency_rates_and_dates('USD', 1)
+        self.assertEqual(rates, [3.7677])
 
-        rates, _ = currency_rates_dates('JOD', 1)
-        self.assertEqual(rates, [])
+        rates, _ = currency_rates_and_dates('JOD', 7)
+        self.assertEqual(len(rates), 7)
 
-        rates, _ = currency_rates_dates('JOD', 7)
-        self.assertEqual(len(rates), 1)
+        rates, dates = currency_rates_and_dates('USD', 7)
+        self.assertEqual(len(rates), len(dates))
+
+        rates, dates = currency_rates_and_dates('USD', 400)
+        self.assertLess(len(rates), 366)
 
     def test_currency_rates_invalid_values(self):
-        rates, _ = currency_rates_dates('USD', 0)
+        rates, _ = currency_rates_and_dates('USD', 0)
         self.assertEqual(rates, [])
 
-        rates, _ = currency_rates_dates('USD', -1)
+        rates, _ = currency_rates_and_dates('USD', -1)
         self.assertEqual(rates, [])
 
-        rates, _ = currency_rates_dates('dgdd', 1)
+        rates, _ = currency_rates_and_dates('dgdd', 1)
         self.assertEqual(rates, [])
 
-        rates, _ = currency_rates_dates('', 1)
+        rates, _ = currency_rates_and_dates('', 1)
         self.assertEqual(rates, [])
 
 
