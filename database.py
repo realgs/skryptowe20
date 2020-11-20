e226e5f7090f6b01e00a2e8505a98d74a41647a4
Pawel <mrprzyprawa@gmail.com>
2020-11-20 14:02:47 +0100

    Add create table in db, insert rates, add missing rates

---
+++ database.py
@@ -0,0 +1,71 @@
+from datetime import datetime, timedelta
+import sqlite3
+import json
+from nbp import fetch_currency_from_two_tables, print_json, from_json_to_list
+
+DB_NAME = "Source/AdventureWorksLT.db"
+DATEFORMAT = "%Y-%m-%d"
+TIME_DELTA = 182
+
+
+def create_avg_currency_rates_table(conn):
+    c = conn.cursor()
+    c.execute(
+    '''
+    CREATE TABLE if not exists AvgUsdRates (
+        avg_rates REAL NOT NULL,
+        date DATE PRIMARY KEY NOT NULL
+    )
+    '''
+    )
+    conn.commit()
+
+
+def insert_usd_rates(conn, rates):
+    c = conn.cursor()
+    c.executemany('INSERT INTO AvgUsdRates VALUES (?, ?)', rates)
+    conn.commit()
+
+
+def add_missing_dates(rates):
+    for i in range(len(rates) - 1):
+        curr = rates[i]
+        next = rates[i + 1]
+
+        curr_date = datetime.strptime(curr[1], DATEFORMAT)
+        next_date = datetime.strptime(next[1], DATEFORMAT)
+        delta = next_date - curr_date
+
+        if delta.days > 1:
+            next_day = curr_date + timedelta(days=1)
+            rates.insert(i + 1, (
+                curr[0],
+                next_day.strftime(DATEFORMAT)
+            ))
+    return rates
+
+
+def get_sales_usd_pln(conn):
+    c = conn.cursor()
+    c.execute(
+        '''
+        SELECT
+        '''
+    )
+
+if __name__ == "__main__":
+    conn = sqlite3.connect(DB_NAME)
+    conn.text_factory = bytes
+
+    # DB OPERATIONS
+    # rates = from_json_to_list(fetch_avg_currency(days=TIME_DELTA))
+    # new_rates = add_missing_dates(rates)
+
+    create_avg_currency_rates_table(conn)
+    # insert_usd_rates(conn, rates)
+
+    c = conn.cursor()
+    c.execute(''' SELECT DISTINCT OrderDate, ShipDate, ModifiedDate FROM SalesOrderHeader''')
+    print(c.fetchall(), sep='\n')
+
+    conn.close()
--- nbp.py
+++ nbp.py
@@ -4,48 +4,70 @@ import numpy as np
 import requests
 import json
 
-def fetch_avg_currency(currency='usd', days=10):
-       req = requests.get(f"http://api.nbp.pl/api/exchangerates/rates/a/{currency}/last/{days}/?format=json")
-       res = req.json()
-       return res
+
+def fetch_avg_currency(table='a', prev_date='2012-01-01', curr_date='2012-01-31', currency='usd'):
+    req = requests.get(f"http://api.nbp.pl/api/exchangerates/rates/{table}/{currency}/{prev_date}/{curr_date}")
+    if req.status_code == 200:
+        res = req.json()
+        return res
+    else:
+        return json.loads('{}')
+
+
+def fetch_currency_from_two_tables( prev_date='2012-01-01', curr_date='2012-01-31', currency='usd'):
+    tab_a = fetch_avg_currency(prev_date=prev_date, curr_date=curr_date, currency=currency)
+    tab_b = fetch_avg_currency(table='b', prev_date=prev_date, curr_date=curr_date, currency=currency)
+
+    return from_json_to_list(tab_a) + from_json_to_list(tab_b)
 
 
 def print_json(json_file):
-       print(json.dumps(json_file, indent=4, sort_keys=True))
+    print(json.dumps(json_file, indent=4, sort_keys=True))
 
 
-def plot(x1, y1, x2, y2, label1, label2, xlabel='', ylabel='', title=''):
-       fig, ax = plt.subplots()
-       ax.plot(x1,y1, label=label1)
-       ax.plot(x2, y2, label=label2)
-       ax.set(xlabel=xlabel, ylabel=ylabel,
-              title=title)
-       ax.set_xticks(x1[::10])
-       ax.legend()
-       plt.xticks(rotation=40)
-       fig.savefig(f'plots/{title}.svg')
-       plt.show()
+def plot(x1, y1, x2, y2, label1='', label2='', xlabel='', ylabel='', title=''):
+    fig, ax = plt.subplots()
+    ax.plot(x1,y1, label=label1)
+    ax.plot(x2, y2, label=label2)
+    ax.set(xlabel=xlabel, ylabel=ylabel,
+            title=title)
+    ax.set_xticks(x1[::10])
+    ax.legend()
+    plt.xticks(rotation=40)
+    fig.savefig(f'plots/{title}.svg')
+    plt.show()
 
 
-avg_usd = fetch_avg_currency(days=182)
-avg_euro = fetch_avg_currency('eur', 182)
-print_json(avg_usd)
-print_json(avg_euro)
+def from_json_to_list(json_file):
+    print_json(json_file)
+    if 'rates' in json_file:
+        mids = [x['mid'] for x in json_file['rates']]
+        dates = [x['effectiveDate'] for x in json_file['rates']]
+        return list(zip(mids, dates))
+    else:
+        return []
 
-usd_mids = [x['mid'] for x in avg_usd['rates']]
-usd_dates = [x['effectiveDate'] for x in avg_usd['rates']]
 
-euro_mids = [x['mid'] for x in avg_euro['rates']]
-euro_dates = [x['effectiveDate'] for x in avg_euro['rates']]
+if __name__ == "__main__":
+    date1 ='2020-05-01'
+    date2 = '2020-11-20'
 
-plot(
-       usd_dates,
-       usd_mids,
-       euro_dates,
-       euro_mids,
-       'USD',
-       'EUR',
-       xlabel='days',
-       ylabel='avg currency',
-       title='Average currency values (EUR, USD) in 182 days'
-)
+    avg_usd = fetch_currency_from_two_tables(date1, date2)
+    usd_mids = [x[0] for x in avg_usd]
+    usd_dates = [x[1] for x in avg_usd]
+
+    avg_euro = fetch_currency_from_two_tables(date1, date2, 'eur')
+    euro_mids = [x[0] for x in avg_euro]
+    euro_dates = [x[1] for x in avg_euro]
+
+    plot(
+        usd_dates,
+        usd_mids,
+        euro_dates,
+        euro_mids,
+        'USD',
+        'EUR',
+        xlabel='days',
+        ylabel='avg currency',
+        title=f'Average currency values (EUR, USD) from {date1} to {date2}'
+    )
