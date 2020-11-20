f3427f9059d75edd16e88c00dbb07c00911e69de
Kamil <kogol99@gmail.com>
2020-11-20 23:19:40 +0100

    fix zad1

--- lab4/currency.py
+++ lab4/currency.py
@@ -1,8 +1,6 @@
 import datetime as dt
 import requests
-from datetime import date, timedelta
-import json
-
+from datetime import date
 
 API_URL = "http://api.nbp.pl/api/exchangerates/rates/"
 
@@ -10,39 +8,39 @@ API_URL = "http://api.nbp.pl/api/exchangerates/rates/"
 def get_listing_courses(currency, number_of_days):
     date_from = date.today() - dt.timedelta(number_of_days)
     date_to = dt.date.today()
-    x = requests.get(API_URL + "a/" + str(currency) + "/" + str(date_from) + "/" + str(date_to) + "/?format=json").json()
-    y = {'number_of_days': number_of_days, 'date_from': datetime_konwerter(str(date_from)),
-         'date_to': datetime_konwerter(str(date_to))}
-    x.update(y)
-    return x
+    api_json_result = requests.get(API_URL + "a/" + str(currency) + "/" + str(date_from) + "/" + str(date_to)
+                                   + "/?format=json").json()
+    additional_content = {'number_of_days': number_of_days, 'date_from': datetime_converter(str(date_from)),
+                          'date_to': datetime_converter(str(date_to))}
+    api_json_result.update(additional_content)
+    return api_json_result
 
 
-def get_tablice_kursow(json):
-    date = []
-    mid = []
-    kolejna_data = 0
+def get_rate_list(json):
+    date_list = []
+    mid_rate_list = []
+    number_of_next_date = 0
     for data in json["rates"]:
-        actual_date = datetime_konwerter(data["effectiveDate"])
-        if len(date) != 0:
-            next_date_object = date[len(date)-1] + dt.timedelta(days=1)
-            while (next_date_object != actual_date) & (not kolejna_data == json["number_of_days"]-1):
-                date.append(next_date_object)
-                mid.append(mid[len(mid) - 1])
+        actual_date = datetime_converter(data["effectiveDate"])
+        if len(date_list) != 0:
+            next_date_object = date_list[len(date_list)-1] + dt.timedelta(days=1)
+            while (next_date_object != actual_date) & (not number_of_next_date == json["number_of_days"]-1):
+                date_list.append(next_date_object)
+                mid_rate_list.append(mid_rate_list[len(mid_rate_list) - 1])
                 next_date_object += dt.timedelta(days=1)
-                kolejna_data += 1
+                number_of_next_date += 1
         else:
             date_before = json["date_from"]
             while date_before < actual_date:
-                date.append(date_before)
-                mid.append(data["mid"])
+                date_list.append(date_before)
+                mid_rate_list.append(data["mid"])
                 date_before += dt.timedelta(days=1)
-                kolejna_data += 1
-        date.append(actual_date)
-        mid.append(data["mid"])
-        kolejna_data += 1
+                number_of_next_date += 1
+        date_list.append(actual_date)
+        mid_rate_list.append(data["mid"])
+        number_of_next_date += 1
+    return date_list, mid_rate_list
 
-    return date, mid
 
-
-def datetime_konwerter(date):
-    return dt.datetime.strptime(date, '%Y-%m-%d')
+def datetime_converter(string_date):
+    return dt.datetime.strptime(string_date, '%Y-%m-%d')
