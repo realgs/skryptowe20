a4e8939cffe2093fec13524488a8f0b1698b0714
Norbert Nawrotek <norbertn15@gmail.com>
2020-11-20 19:18:54 +0100

    Refactor code responsible for drawing exchange_rates.svg

--- plot_drawer.py
+++ plot_drawer.py
@@ -25,15 +25,14 @@ def create_plot_from_database(database_data, code_currency_original, code_curren
 
 
 def create_plot_from_exchange_rates(rates_currency_1, code_currency_1, rates_currency_2, code_currency_2):
-    x_val_usd = [rate[0] for rate in rates_currency_1]
+    x_val = [rate[0] for rate in rates_currency_1]
     y_val_usd = [rate[1] for rate in rates_currency_1]
-    x_val_eur = [rate[0] for rate in rates_currency_2]
     y_val_eur = [rate[1] for rate in rates_currency_2]
 
-    plt.plot(x_val_usd, y_val_usd)
-    plt.plot(x_val_eur, y_val_eur)
+    plt.plot(x_val, y_val_usd)
+    plt.plot(x_val, y_val_eur)
     plt.xticks(rotation=50)
-    plt.xticks(ticks=np.arange(0, len(x_val_usd), step=len(x_val_usd) / 9))
+    plt.xticks(ticks=np.arange(0, len(x_val), step=len(x_val) / 9))
 
     plt.title("Exchange rate for each available day")
     plt.xlabel("Date")
