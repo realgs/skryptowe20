97480804aead8fcde59f44a0a52bc68a8a3e2121
limisie <k.limisiewicz@gmail.com>
2020-11-20 18:51:55 +0100

    Add plot_sales and refactor

--- L4_API/db_handler.py
+++ L4_API/db_handler.py
@@ -1,14 +1,11 @@
 import sqlite3
-from datetime import datetime, timedelta
-
 import matplotlib.pyplot as plt
-from matplotlib import dates
 
 DB_FILE = 'sales.db'
 PLOT_TICKS = 30
-WIDTH = 0.1
 PLOT_SIZE_X = 10
 PLOT_SIZE_Y = 5
+PLOT_SAVE = False
 
 
 def connect_db():
@@ -63,6 +60,52 @@ def get_rate(date, currency_code):
     return rate
 
 
+def get_rates_and_dates(currency_code, date_from, date_to):
+    conn = connect_db()
+    cursor = conn.cursor()
+    rates = []
+    dates = []
+
+    cursor.execute("""SELECT Rate, RateDate FROM rates
+                    WHERE RateDate BETWEEN '{}' AND '{}'
+                    AND Code = '{}';""".format(date_from, date_to, currency_code))
+    try:
+        for rate, date in cursor.fetchall():
+            rates.append(float(rate))
+            dates.append(date)
+    except TypeError as e:
+        print('db_handler: get_rates' + str(e))
+
+    conn.close()
+
+    return rates, dates
+
+
+def get_sales_and_dates(date_from, date_to):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    sales = []
+    dates = []
+
+    date_from += ' 00:00:00'
+    date_to += ' 00:00:00'
+
+    cursor.execute("""SELECT SUM(Total), InvoiceDate FROM invoices
+                             WHERE InvoiceDate BETWEEN '{}' AND '{}'
+                             GROUP BY InvoiceDate""".format(date_from, date_to))
+    try:
+        for sale, date in cursor.fetchall():
+            sales.append(float(sale))
+            dates.append(date[:10])
+    except TypeError as e:
+        print('db_handler: get_sales' + str(e))
+
+    conn.close()
+
+    return sales, dates
+
+
 def create_rates_table():
     conn = connect_db()
     cursor = conn.cursor()
@@ -88,29 +131,28 @@ def add_rate_entry(date, rate, currency_code):
     conn.close()
 
 
-def plot_data(currency_code, date_from, date_to):
-    dates = []
-    sales = []
-    rates = []
+def data_to_plot(currency_code, date_from, date_to):
+    rates, dates = get_rates_and_dates(currency_code, date_from, date_to)
+    sales, sale_dates = get_sales_and_dates(date_from, date_to)
+    sales_usd = []
     sales_pln = []
 
-    date = date_from
-    while date <= date_to:
-        sale = get_total_sale(date)
-        rate = float(get_rate(date, currency_code))
-        dates.append(date)
-        sales.append(sale)
-        rates.append(rate)
-        sales_pln.append(sale * rate)
+    sale_index = 0
 
-        new_date = datetime.strptime(date, '%Y-%m-%d') + timedelta(days=1)
-        date = new_date.strftime('%Y-%m-%d')
+    for i in range(len(dates)):
+        if sale_index < len(sales) and dates[i] == sale_dates[sale_index]:
+            sales_usd.append(sales[sale_index])
+            sales_pln.append(sales[sale_index] * rates[i])
+            sale_index += 1
+        else:
+            sales_usd.append(0)
+            sales_pln.append(0)
 
-    return dates, sales, rates, sales_pln
+    return dates, sales_usd, sales_pln
 
 
-def plot(currency_code, date_from, date_to):
-    sale_dates, sales, rates, sales_pln = plot_data(currency_code, date_from, date_to)
+def plot_sale_time_frame(currency_code, date_from, date_to):
+    sale_dates, sales_usd, sales_pln = data_to_plot(currency_code, date_from, date_to)
     x_min = date_from
     x_max = date_to
 
@@ -118,7 +160,7 @@ def plot(currency_code, date_from, date_to):
     plt.subplots_adjust(left=0.1, bottom=0.2)
 
     plt.plot(sale_dates, sales_pln, linewidth=0.8, label='PLN')
-    plt.plot(sale_dates, sales, linewidth=0.8, label='USD')
+    plt.plot(sale_dates, sales_usd, linewidth=0.8, label='USD')
 
     plt.title("Sprzedaż w dniach od {} do {}".format(x_min, x_max))
     plt.xlabel("data")
@@ -135,6 +177,8 @@ def plot(currency_code, date_from, date_to):
             tick.set_visible(False)
 
     plt.show()
+    if PLOT_SAVE:
+        plt.savefig('sales.svg')
 
 
 if __name__ == '__main__':
--- L4_API/main.py
+++ L4_API/main.py
@@ -19,7 +19,7 @@ def plot_rates(currency_codes, days):
 
 
 def plot_sales(currency_code, date_from, date_to):
-    db_hdl.plot(currency_code, date_from, date_to)
+    db_hdl.plot_sale_time_frame(currency_code, date_from, date_to)
 
 
 def get_rates(currency_code, days):
@@ -35,14 +35,14 @@ def add_rates_to_db(currency_code, date_from, date_to):
 
 
 if __name__ == '__main__':
-    # for currency in CURRENCIES:
-    #     print(get_rates(currency, YEAR // 2))
-    #
-    # plot(CURRENCIES, YEAR // 2)
+    for currency in CURRENCIES:
+        print(get_rates(currency, YEAR // 2))
 
-    # add_rates_to_db('USD', '2008-12-20', '2009-12-31')
-    # add_rates_to_db('USD', '2009-12-20', '2010-12-31')
-    # add_rates_to_db('USD', '2010-12-20', '2011-12-31')
-    # add_rates_to_db('USD', '2011-12-20', '2012-12-31')
-    # add_rates_to_db('USD', '2012-12-20', '2013-12-31')
-    plot_sales('USD', '2009-01-01', '2009-12-31')
+    plot_rates(CURRENCIES, YEAR // 2)
+
+    # add_rates_to_db('USD', '2009-01-01', '2009-12-31')
+    # add_rates_to_db('USD', '2010-01-01', '2010-12-31')
+    # add_rates_to_db('USD', '2011-01-01', '2011-12-31')
+    # add_rates_to_db('USD', '2012-01-01', '2012-12-31')
+    # add_rates_to_db('USD', '2013-01-01', '2013-12-31')
+    plot_sales('USD', '2010-01-01', '2010-12-31')
