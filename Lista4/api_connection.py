57b24ade0ac572b8656028af8b10d59687dac7b2
Grzegorz Myjak <g-myjak@wp.pl>
2020-11-20 20:36:32 +0100

    Added task 4 and helper function in api file

--- Lista4/api_connection.py
+++ Lista4/api_connection.py
@@ -20,6 +20,24 @@ def get_average_currency_rates(currency, days):
     return result
 
 
+# Funkcja przygotowana pod uzupenianie bazy danych z zadania 4
+def get_average_currency_rates_between(currency, first_day, last_day):
+    url = 'http://api.nbp.pl/api/exchangerates/rates/A/' + currency + '/'
+    result = []
+    end_date = last_day
+    days = (last_day - first_day).days
+
+    while days > 0:
+        begin_date = end_date - datetime.timedelta(days=min(days, 92))
+        response = requests.get(url + begin_date.strftime('%Y-%m-%d') + '/' + end_date.strftime('%Y-%m-%d'))
+        if response.status_code == 200:
+            result = response.json()['rates'] + result
+        days -= 93
+        end_date = end_date - datetime.timedelta(days=93)
+
+    return result
+
+
 #Zadanie 3 - Rysowanie wykresu
 def draw_currency_diagram(dollar_data, euro_data):
     data_time = []
@@ -49,3 +67,5 @@ if __name__ == '__main__':
     half_year_euro_averages = get_average_currency_rates('EUR', 183)
 
     draw_currency_diagram(half_year_dollar_averages, half_year_euro_averages)
+
+    print(get_average_currency_rates_between('USD', datetime.date(2019, 3, 12), datetime.date(2020, 3, 12)))
--- Lista4/db_connection.py
+++ Lista4/db_connection.py
@@ -1,13 +1,39 @@
 import pyodbc as db
+import datetime
+from api_connection import get_average_currency_rates_between
+
+
+# Zadanie 4 - modyfikacja istniejcej bazy danych
+def create_rates_table(data):
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE BikeStores.dbo.rates ('
+                   'measure_date DATE NOT NULL PRIMARY KEY,'
+                   'measure_rate DECIMAL (7, 4) NOT NULL);')
+
+    query = 'INSERT INTO BikeStores.dbo.rates(measure_date,measure_rate) VALUES(?,?)'
+
+    for idx in range(len(data)):
+        cursor.execute(query, (data[idx]['effectiveDate'], float(data[idx]['mid'])))
+        if idx < len(data) - 1:
+            date_idx0 = datetime.datetime.strptime(data[idx]['effectiveDate'], '%Y-%m-%d')
+            date_idx1 = datetime.datetime.strptime(data[idx+1]['effectiveDate'], '%Y-%m-%d')
+            date_diff = (date_idx1 - date_idx0).days
+            for excess in range(date_diff - 1):
+                date_idx0 += datetime.timedelta(days=1)
+                cursor.execute(query, (date_idx0, float(data[idx]['mid'])))
+
+    conn.commit()
+
 
 if __name__ == '__main__':
+    # Database created with:
+    # https://www.sqlservertutorial.net/sql-server-sample-database/
     conn = db.connect('Driver={SQL Server};'
                       'Server=DESKTOP-AKTNFDK;'
-                      'Database=AdventureWorksLT2019;'
+                      'Database=BikeStores;'
                       'Trusted_Connection=yes;')
 
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM AdventureWorksLT2019.SalesLT.SalesOrderHeader')
-
-    for row in cursor:
-        print(row)
+    #for row in cursor:
+    #    print(row)
+    table_data = get_average_currency_rates_between('USD', datetime.date(2015, 12, 20), datetime.date(2018, 1, 4))
+    create_rates_table(table_data)
