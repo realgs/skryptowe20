110165dd4055e780ba73c7f9e5f0c07c831f1e66
Norbert Nawrotek <norbertn15@gmail.com>
2020-11-20 17:06:40 +0100

    Create table in database

--- table_manipulator.py
+++ table_manipulator.py
@@ -2,6 +2,8 @@ import sqlite3
 import exchange_rates_acquirer as rates_acquirer
 import datetime
 
+CREATE_TABLE_QUERY = "CREATE TABLE ExchangeRate (date text, rate real)"
+
 
 def get_rows_from_api(currency_code, start_date, end_date):
     api_rates = rates_acquirer.get_exchange_rates_from_api(currency_code, start_date, end_date)
@@ -11,4 +13,11 @@ def get_rows_from_api(currency_code, start_date, end_date):
 
 
 if __name__ == "__main__":
+    connection = sqlite3.connect("Northwind_large.sqlite")
+    cursor = connection.cursor()
+    cursor.execute(CREATE_TABLE_QUERY)
+
     rows = get_rows_from_api("usd", datetime.date(2013, 1, 1), datetime.date(2015, 12, 31))
+
+    cursor.close()
+    connection.close()
