53e0ed8d8c4341f5a358a7f6d6e0e4e184d3f64d
szawruk <szawruk@gmail.com>
2020-11-21 01:45:58 +0100

    upd function fetching exchange rates of currency

--- lab4/rozwiazanie/lab4.py
+++ lab4/rozwiazanie/lab4.py
@@ -12,7 +12,7 @@ MAX_DAYS_FOR_QUERY = 300
 
 def get_currency_rates_from_x_last_days(currency, number_of_days):
     today_date = date.today()
-    date_x_days_ago = today_date - timedelta(days=number_of_days - 1)
+    date_x_days_ago = today_date - timedelta(days=number_of_days)
     return get_currency_rates_from_date_range(currency, date_x_days_ago, today_date)
 
 
@@ -25,15 +25,14 @@ def get_currency_rates_from_date_range(currency, start_date, end_date):
 
     left_days_to_fetch = (end_date - start_date).days
     while left_days_to_fetch > 0:
-        number_of_days_to_fetch_now = left_days_to_fetch % MAX_DAYS_FOR_QUERY
+        number_of_days_to_fetch_now = MAX_DAYS_FOR_QUERY if left_days_to_fetch >= MAX_DAYS_FOR_QUERY else left_days_to_fetch
         left_days_to_fetch -= number_of_days_to_fetch_now
         temp_end_date = start_date + timedelta(days=number_of_days_to_fetch_now)
-
         data = requests.get(r'http://api.nbp.pl/api/exchangerates/rates/a/' + currency + '/' + str(start_date) + '/' + str(temp_end_date) + '/')
         data_len = len(json.loads(data.text)['rates'])
         exchange_rates = [json.loads(data.text)['rates'][i]['mid'] for i in range(data_len)]
         nbp_dates = [json.loads(data.text)['rates'][i]['effectiveDate'] for i in range(data_len)]
-        all_dates = [str(start_date + timedelta(days=i)) for i in range(number_of_days_to_fetch_now + 1)]
+        all_dates = [str(start_date + timedelta(days=i)) for i in range(number_of_days_to_fetch_now - 1)]
 
         # complete empty dates for weekends and breaks
         for i in range(len(all_dates)):
@@ -45,29 +44,33 @@ def get_currency_rates_from_date_range(currency, start_date, end_date):
                 nbp_dates.insert(i, all_dates[i])
 
         rates += exchange_rates
-    print(len(rates))
+        start_date = temp_end_date
     return rates
 
 
-# exercise 2----------------------------------------------------------------------------
-# half a year - 183 days
-dollar = get_currency_rates_from_x_last_days('usd', 183)
-print(dollar)
-euro = get_currency_rates_from_x_last_days('eur', 183)
+def print_plot_for_dollar_euro_x_days(days):
+    dollar = get_currency_rates_from_x_last_days('usd', days)
+    euro = get_currency_rates_from_x_last_days('eur', days)
+    today_date = date.today()
+    x__days_ago_date = today_date - timedelta(days=days)
+    dates = mdates.drange(x__days_ago_date, today_date, timedelta(days=1))
+    print_plot_for_currencies(dollar, euro, dates)
 
-# exercise 3----------------------------------------------------------------------------
-today_date = date.today()
-x__days_ago_date = today_date - timedelta(days=len(dollar))
-days = mdates.drange(x__days_ago_date, today_date, timedelta(days=1))
 
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=20))
-plt.plot(days, dollar, days, euro)
-plt.gcf().autofmt_xdate()
+def print_plot_for_currencies(dollar, euro, dates):
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=int(len(dates) / 10)))
+    plt.plot(dates, dollar, dates, euro)
+    plt.gcf().autofmt_xdate()
 
-plt.xlabel('czas')
-plt.ylabel('średni kurs notowań [zł]')
-plt.title('Zależność notowań dolara i euro od czasu')
-plt.legend(['USD', 'EUR'], loc='best')
-plt.savefig('dollar_euro_exchange_rates_chart.svg')
-plt.show()
+    plt.xlabel('czas')
+    plt.ylabel('średni kurs notowań [zł]')
+    plt.title('Zależność notowań dolara i euro od czasu')
+    plt.legend(['USD', 'EUR'], loc='best')
+    plt.savefig('dollar_euro_exchange_rates_chart.svg')
+    plt.show()
+
+
+if __name__ == '__main__':
+    print_plot_for_dollar_euro_x_days(183)
+
