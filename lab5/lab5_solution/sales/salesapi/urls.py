d662010dfc04161cd410232773ecb57d326c8cf3
kwarc-agat <aga.kwarc@gmail.com>
2020-12-06 17:35:31 +0100

    add view for ex.1

--- lab5/lab5_solution/sales/salesapi/urls.py
+++ lab5/lab5_solution/sales/salesapi/urls.py
@@ -8,5 +8,7 @@ router.register(r'sales', views.DailySalesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('rate/<str:one_date>', views.getJsonRate),
+    path('rate/<str:from_date>/<str:to_date>', views.getJsonRateTwoDates)
 ]
--- lab5/lab5_solution/sales/salesapi/views.py
+++ lab5/lab5_solution/sales/salesapi/views.py
@@ -16,3 +16,40 @@ class CurrencyViewSet(viewsets.ModelViewSet):
 class DailySalesViewSet(viewsets.ModelViewSet):
     queryset = DailySales.objects.all().order_by('sales_id')
     serializer_class = DailySalesSerializer
+
+def getJsonRate(request, one_date):
+    if isBelowTimeLimit(request):
+        try:
+            response = CurrencyRecord.objects.get(effectivedate=one_date)
+        except CurrencyRecord.DoesNotExist:
+            return JsonResponse({"unrecognized_url": 1})
+
+        return JsonResponse({one_date: response.currencyvalue})
+    else:
+        return HttpResponse(status=404)
+
+def getJsonRateTwoDates(request, from_date, to_date):
+    if isBelowTimeLimit(request):
+        response = {}
+        missing_records=0
+        try:
+            from_object = CurrencyRecord.objects.get(effectivedate=from_date)
+            to_object = CurrencyRecord.objects.get(effectivedate=to_date)
+        except CurrencyRecord.DoesNotExist:
+            return JsonResponse({"unrecognized_url": 1})
+
+        if from_object.id > to_object.id:
+            ids_range = range(to_object.id, from_object.id)
+        else:
+            ids_range = range(from_object.id, to_object.id)
+
+        for i in ids_range:
+            try:
+                record = CurrencyRecord.objects.get(id=i)
+                response[record.effectivedate]=record.currencyvalue
+            except CurrencyRecord.DoesNotExist:
+                missing_records+=1
+                response["missing_records"]=missing_records
+        return JsonResponse(response)
+    else:
+        return HttpResponse(status=404)
