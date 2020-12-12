e59d3ca107e1279d905174cd7f5f2cdd30abf212
Kacper <kacper.szkodzinski@outlook.com>
2020-12-12 10:09:26 +0100

    interpolated and sales summary

--- my_little_api/dvd_rental_api.py
+++ my_little_api/dvd_rental_api.py
@@ -19,6 +19,7 @@ Base.prepare(conn, reflect=True)
 
 # cache
 exchange_rates_cache = {}
+payments_cache = {}
 min_date = None
 max_date = None
 
@@ -33,7 +34,7 @@ def api_exchangerates():
     start_date = None
     end_date = None
     results = {}
-    index = 1
+    index = 0
     is_correct = True
 
     # Get Variables
@@ -43,6 +44,9 @@ def api_exchangerates():
             if start_date < min_date:
                 results['error_001'] = "DECLARED START DATE LOWER THAN MIN DATE. OMIT PARAMETER TO USE MIN DATE."
                 is_correct = False
+            if start_date > max_date:
+                results['error_002'] = "DECLARED START DATE HIGHER THAN MAX DATE. OMIT PARAMETER TO USE MIN DATE."
+                is_correct = False
         except ValueError:
             results['error_003'] = "WRONG START DATE FORMAT. PLEASE USE DD/MM/YY FORMAT."
             is_correct = False
@@ -50,11 +54,17 @@ def api_exchangerates():
     if 'enddate' in request.args:
         try:
             end_date = datetime.strptime(request.args['enddate'], '%d/%m/%y').date()
+            if end_date < min_date:
+                results['error_004'] = "DECLARED END DATE LOWER THAN MIN DATE. OMIT PARAMETER TO USE MAX DATE."
+                is_correct = False
             if end_date > max_date:
-                results['error_002'] = "DECLARED END DATE HIGHER THAN MAX DATE. OMIT PARAMETER TO USE MAX DATE."
+                results['error_005'] = "DECLARED END DATE HIGHER THAN MAX DATE. OMIT PARAMETER TO USE MAX DATE."
+                is_correct = False
+            if end_date < start_date:
+                results['error_006'] = "DECLARED END DATE LOWER THAN MIN DATE. OMIT PARAMETER TO USE MAX DATE."
                 is_correct = False
         except ValueError:
-            results['error_004'] = "WRONG END DATE FORMAT. PLEASE USE DD/MM/YY FORMAT."
+            results['error_007'] = "WRONG END DATE FORMAT. PLEASE USE DD/MM/YY FORMAT."
             is_correct = False
 
     # Process
@@ -62,20 +72,17 @@ def api_exchangerates():
         if start_date is not None:
             if end_date is not None:
                 for i in range(exchange_rates_cache.__len__()):
-                    test = exchange_rates_cache[i]
                     if start_date <= exchange_rates_cache[i]['date'] <= end_date:
                         results[index] = exchange_rates_cache[i]
                         index += 1
             else:
                 for i in range(exchange_rates_cache.__len__()):
-                    test = exchange_rates_cache[i]
                     if start_date <= exchange_rates_cache[i]['date']:
                         results[index] = exchange_rates_cache[i]
                         index += 1
         else:
             if end_date is not None:
                 for i in range(exchange_rates_cache.__len__()):
-                    test = exchange_rates_cache[i]
                     if exchange_rates_cache[i]['date'] <= end_date:
                         results[index] = exchange_rates_cache[i]
                         index += 1
@@ -86,11 +93,129 @@ def api_exchangerates():
     return jsonify(results)
 
 
+@app.route('/api/v1/resources/salessummary', methods=['GET'])
+def api_salessummary():
+    results = {'COUNT': 0, 'SUM_IN_USD': 0, 'SUM_IN_PLN': 0}
+    date = None
+    is_correct = True
+
+    # Get Variables
+    if 'date' in request.args:
+        try:
+            date = datetime.strptime(request.args['date'], '%d/%m/%y').date()
+            if date < min_date:
+                results['error_008'] = "DECLARED DATE LOWER THAN MIN DATE. OMIT PARAMETER TO GET LATEST SUMMARY."
+                is_correct = False
+            if date > max_date:
+                results['error_009'] = "DECLARED DATE HIGHER THAN MAX DATE. OMIT PARAMETER TO GET LATEST SUMMARY."
+                is_correct = False
+        except ValueError:
+            results['error_010'] = "WRONG DATE FORMAT. PLEASE USE DD/MM/YY FORMAT."
+            is_correct = False
+
+    # Process
+    if is_correct:
+        if date is not None:
+            for i in range(payments_cache.__len__()):
+                if payments_cache[i]['payment_date'] == date:
+                    results['COUNT'] += 1
+                    results['SUM_IN_USD'] += payments_cache[i]['amount']
+                    results['SUM_IN_PLN'] += payments_cache[i]['amount_in_PLN']
+
+    # Return
+    return jsonify(results)
+
+
+@app.route('/api/v1/resources/salessummary/range', methods=['GET'])
+def api_salessummaryrange():
+    start_date = None
+    end_date = None
+    results = {'COUNT': 0, 'SUM_IN_USD': 0, 'SUM_IN_PLN': 0}
+    is_correct = True
+
+    # Get Variables
+    if 'startdate' in request.args:
+        try:
+            start_date = datetime.strptime(request.args['startdate'], '%d/%m/%y').date()
+            if start_date < min_date:
+                results['error_001'] = "DECLARED START DATE LOWER THAN MIN DATE. OMIT PARAMETER TO USE MIN DATE."
+                is_correct = False
+            if start_date > max_date:
+                results['error_002'] = "DECLARED START DATE HIGHER THAN MAX DATE. OMIT PARAMETER TO USE MIN DATE."
+                is_correct = False
+        except ValueError:
+            results['error_003'] = "WRONG START DATE FORMAT. PLEASE USE DD/MM/YY FORMAT."
+            is_correct = False
+
+    if 'enddate' in request.args:
+        try:
+            end_date = datetime.strptime(request.args['enddate'], '%d/%m/%y').date()
+            if end_date < min_date:
+                results['error_004'] = "DECLARED END DATE LOWER THAN MIN DATE. OMIT PARAMETER TO USE MAX DATE."
+                is_correct = False
+            if end_date > max_date:
+                results['error_005'] = "DECLARED END DATE HIGHER THAN MAX DATE. OMIT PARAMETER TO USE MAX DATE."
+                is_correct = False
+            if end_date < start_date:
+                results['error_006'] = "DECLARED END DATE LOWER THAN MIN DATE. OMIT PARAMETER TO USE MAX DATE."
+                is_correct = False
+        except ValueError:
+            results['error_007'] = "WRONG END DATE FORMAT. PLEASE USE DD/MM/YY FORMAT."
+            is_correct = False
+
+    # Process
+    if is_correct:
+        if start_date is not None:
+            if end_date is not None:
+                for i in range(payments_cache.__len__()):
+                    if start_date <= payments_cache[i]['payment_date'] <= end_date:
+                        results['COUNT'] += 1
+                        results['SUM_IN_USD'] += payments_cache[i]['amount']
+                        results['SUM_IN_PLN'] += payments_cache[i]['amount_in_PLN']
+            else:
+                for i in range(payments_cache.__len__()):
+                    if start_date <= payments_cache[i]['payment_date']:
+                        results['COUNT'] += 1
+                        results['SUM_IN_USD'] += payments_cache[i]['amount']
+                        results['SUM_IN_PLN'] += payments_cache[i]['amount_in_PLN']
+        else:
+            if end_date is not None:
+                for i in range(payments_cache.__len__()):
+                    if payments_cache[i]['payment_date'] <= end_date:
+                        results['COUNT'] += 1
+                        results['SUM_IN_USD'] += payments_cache[i]['amount']
+                        results['SUM_IN_PLN'] += payments_cache[i]['amount_in_PLN']
+            else:
+                for i in range(payments_cache.__len__()):
+                    results['COUNT'] += 1
+                    results['SUM_IN_USD'] += payments_cache[i]['amount']
+                    results['SUM_IN_PLN'] += payments_cache[i]['amount_in_PLN']
+
+    # Return
+    return jsonify(results)
+
+
 def on_start():
+    # We are refreshing whole cache instead of just appending new dates, to update old values in case of any edits.
     Session = sessionmaker(bind=conn)
     session = Session()
     global exchange_rates_cache
-    exchange_rates_cache = to_dictionary.model_to_dict(session.query(Base.classes.exchange_rate).all())
+    exchange_rates_cache = to_dictionary.model_to_dict(session.query(Base.classes.exchange_rate.date,
+                                                                     Base.classes.exchange_rate.usd_to_pln,
+                                                                     Base.classes.exchange_rate.interpolated).all())
+    global payments_cache
+    payments_cache = to_dictionary.model_to_dict(session.query(Base.classes.payment.amount,
+                                                               Base.classes.payment.payment_date).all())
+    for i in range(payments_cache.__len__()):
+        exchange_rate_found = False
+        payments_cache[i]['amount'] = float(payments_cache[i]['amount'])
+        for j in range(exchange_rates_cache.__len__()):
+            if exchange_rates_cache[j]['date'] == payments_cache[i]['payment_date']:
+                payments_cache[i]['amount_in_PLN'] = payments_cache[i]['amount'] * exchange_rates_cache[j]['usd_to_pln']
+                exchange_rate_found = True
+                break
+        if not exchange_rate_found:
+            payments_cache[i]['amount_in_PLN'] = 0
     global min_date
     min_date = exchange_rates_cache[0]['date']
     global max_date
--- my_little_api/to_dictionary.py
+++ my_little_api/to_dictionary.py
@@ -1,7 +1,6 @@
 def model_to_dict(model):
     list_of_dict = {}
     for i in range(len(model)):
-        dictionary = model[i].__dict__
-        del dictionary["_sa_instance_state"]
+        dictionary = model[i]._asdict()
         list_of_dict[i] = dictionary
     return list_of_dict
