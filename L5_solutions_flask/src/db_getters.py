1dbe82d95f929fede47fbae6a350d253d1ae35fb
kamilagalka <contactkamilagalka@gmail.com>
2020-12-15 12:31:08 +0100

    added sales db getters

--- L5_solutions_flask/src/db_getters.py
+++ L5_solutions_flask/src/db_getters.py
@@ -21,3 +21,17 @@ def get_usd_rating_date_range(start_date, end_date):
     sql_query = f"select effectiveDate, USD, interpolated from USDPrices where effectiveDate between '{start_date}' and '{end_date}'"
     result = pd.read_sql_query(sql_query, conn)
     return result.to_json(orient="records")
+
+
+def get_sales(date):
+    conn = get_db_connection()
+    sql_query = f"""select DATE, USD, PLN from Sales where DATE='{date}'"""
+    result = pd.read_sql_query(sql_query, conn)
+    return result.to_json(orient="records")
+
+
+def get_sales_date_range(start_date, end_date):
+    conn = get_db_connection()
+    sql_query = f"select DATE, USD, PLN from Sales where DATE between '{start_date}' and '{end_date}'"
+    result = pd.read_sql_query(sql_query, conn)
+    return result.to_json(orient="records")
