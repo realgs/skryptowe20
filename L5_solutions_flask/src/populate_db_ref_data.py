1c2a3abd6c0770551c821c9d6e8e884e1245f531
kamilagalka <contactkamilagalka@gmail.com>
2020-12-16 22:51:00 +0100

    fixed calls order

--- L5_solutions_flask/src/populate_db_ref_data.py
+++ L5_solutions_flask/src/populate_db_ref_data.py
@@ -15,10 +15,10 @@ def add_table(conn, table_data: pd.DataFrame, table_name):
     table_data.to_sql(table_name, conn, if_exists='replace')
 
 
-usd_daily_ex_rates_df = get_complete_usd_daily_ex_rates(DATA_DATE_RANGE_START, DATA_DATE_RANGE_END)
-usd_daily_ex_rates_df.sort_values('effectiveDate', inplace=True)
-orders_value_daily_data = get_orders_value_daily(DATA_DATE_RANGE_START, DATA_DATE_RANGE_END)
-
 db_conn = get_db_connection(PATH_TO_DB)
+
+usd_daily_ex_rates_df = get_complete_usd_daily_ex_rates(DATA_DATE_RANGE_START, DATA_DATE_RANGE_END)
 add_table(db_conn, usd_daily_ex_rates_df, USD_EX_RATES_TABLE_NAME)
+
+orders_value_daily_data = get_orders_value_daily(DATA_DATE_RANGE_START, DATA_DATE_RANGE_END)
 add_table(db_conn, orders_value_daily_data, SALES_TABLE_NAME)
