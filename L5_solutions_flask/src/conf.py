61826843a9b67ea0f84470c5b82e946674ee95b4
kamilagalka <contactkamilagalka@gmail.com>
2020-12-16 22:52:14 +0100

    fixed db path

--- L5_solutions_flask/src/conf.py
+++ L5_solutions_flask/src/conf.py
@@ -1,3 +1,6 @@
-PATH_TO_DB = 'Northwind_large.sqlite'
+import os
+
+DB_NAME = 'Northwind_large.sqlite'
+PATH_TO_DB = os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir, DB_NAME))
 DEFAULT_LIMIT = "10/s"
 DEFAULT_CACHE_TIMEOUT = 100
