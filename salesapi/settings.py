e948afbafa8ec18d96032fa7ee2cd649e07cce6f
Jedrzej Rochala <rochala@tutanota.com>
2020-12-17 14:46:15 +0100

    fix django

--- salesapi/settings.py
+++ salesapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^x$dmbixk5dn3af)f#z)f$*3goz8^4n7s0bbav^6ie()3f1dfb'
+SECRET_KEY = '-2g%pr_rjlmzev39w^*k_i#s2lum54rd3nf-+e0%1z65if-__d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
+    'sales.apps.SalesConfig'
 ]
 
 MIDDLEWARE = [
