f4d4d2324abeb399599320cd18415699b9a7ed2e
Jedrzej Rochala <rochala@tutanota.com>
2020-12-17 16:02:08 +0100

    super simple api base

---
+++ sales/serializers.py
@@ -0,0 +1,14 @@
+from sales.models import SalesStats, Currency
+from rest_framework import serializers
+
+
+class CurrencySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Currency
+        fields = ['symbol', 'date', 'value', 'interpolated']
+
+
+class SalesStatsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = SalesStats
+        fields = ['date', 'sales_sum']
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from sales.models import Currency, SalesStats
+from rest_framework import viewsets
+from rest_framework import permissions
+from sales.serializers import CurrencySerializer, SalesStatsSerializer
 
-# Create your views here.
+class CurrencyViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = SalesStats.objects.all().order_by('date')
+    serializer_class = SalesStatsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class SalesStatsViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    permission_classes = [permissions.IsAuthenticated]
