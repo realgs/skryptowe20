959ac17a89090c6a5685e64001fa29c9b769bf9a
Piotrek <pswirkaitis@wp.pl>
2020-12-18 18:50:39 +0100

    small refactor

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import api
 from constans import CACHE_UPDATE_INTERVAL
 
 if __name__ == "__main__":
-
-
     cache.update_cache()
     refresh = BackgroundScheduler()
     refresh.add_job(cache.update_cache, 'interval', hours=CACHE_UPDATE_INTERVAL, replace_existing=True)
@@ -14,4 +12,3 @@ if __name__ == "__main__":
     atexit.register(lambda: refresh.shutdown(wait=False))
 
     api.run()
-
