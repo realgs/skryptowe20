fffaefd776f1558336169f8fed91391714efa255
Jędrzej Rochala <48657087+Rochala@users.noreply.github.com>
2020-12-18 10:07:56 +0100

    Update settings.py

--- salesapi/settings.py
+++ salesapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-2g%pr_rjlmzev39w^*k_i#s2lum54rd3nf-+e0%1z65if-__d'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
