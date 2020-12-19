d65a257b82bc5eae73b191db75ee6c9649204589
joannasroka <joannasroka98@gmail.com>
2020-12-19 16:41:03 +0100

    changed wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import lab5_solutions
 from lab5_solutions.api import app
 import os
+from lab5_solutions.database_repository import *
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
