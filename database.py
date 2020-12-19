ca85d50a9a025f269f20335aee61ca6cf4e4dfef
Konrad <konrad.hajduga@gmail.com>
2020-12-19 19:37:32 +0100

    add interpolation field

--- database.py
+++ database.py
@@ -18,17 +18,12 @@ class DatabaseManager:
         )
         self.__cursor = self.__conn.cursor()
 
-    def read(self, table_name, date_from, date_to):
-        self.__cursor.execute(f"select * from {table_name}"
-                              f" where QuotationDate between '{date_from}' AND '{date_to}'")
-        for row in self.__cursor:
-            print(row)
-
     def create_currency_table(self, table_name):
         self.__cursor.execute(f'''
           CREATE TABLE {table_name} (
                 QuotationDate DATETIME PRIMARY KEY,
-                CurrencyRate REAL NOT NULL
+                CurrencyRate REAL NOT NULL,
+                Interpolated BIT NOT NULL
           );
         ''')
         self.__conn.commit()
@@ -39,58 +34,98 @@ class DatabaseManager:
 
         table_name = self.__get_table_name_for_currency(currency)
         is_table_created = self.__check_currency_table_name_and_create(table_name)
-        currency_data_downloader = CurrencyDataDownloader(currency)
         if is_table_created:
+            currency_data_downloader = CurrencyDataDownloader(currency)
             currency_data = currency_data_downloader.get_currency_prices_for_dates(date_from, date_to)
-
-            dates = []
-            api_data_available_for_dates = currency_data[0] is not None and len(currency_data[0]) > 0
-            if api_data_available_for_dates:
-                oldest_date = datetime.strptime(currency_data[0][0][0], '%Y-%m-%d')
-                rate_date = oldest_date
-                while rate_date.date() <= date_to:
-                    dates.append(rate_date)
-                    rate_date += timedelta(days=1)
-                currency_rates = self.__create_currency_rates_for_dates(currency_data)
-                return self.__insert_currency_data(dates, currency_rates, table_name)
-            else:
-                last_available_rate = self.__get_first_available_rate_from_date(date_from, currency)
-                rate_date = date_from
-                currency_rates = []
-
-                while rate_date <= date_to:
-                    currency_rates.append(last_available_rate)
-                    dates.append(rate_date)
-                    rate_date += timedelta(days=1)
-
-                return self.__insert_currency_data(dates, currency_rates, table_name)
+            return self.__create_rates_and_insert_data(currency_data, date_from, date_to, currency, table_name)
         return None
 
-    def __create_currency_rates_for_dates(self, currency_data):
+    def __create_rates_and_insert_data(self, currency_data, date_from, date_to, currency, table_name):
+        dates = []
+        interpolation_statements = []
+        api_data_available_for_dates = currency_data[0] is not None and len(currency_data[0]) > 0
+        if api_data_available_for_dates:
+            rate_date = date_from
+            while rate_date <= date_to:
+                dates.append(rate_date)
+                rate_date += timedelta(days=1)
+            currency_rates_data = self.__create_currency_rates_for_dates(currency_data, date_from, date_to)
+            currency_rates = currency_rates_data[0]
+            interpolation_statements = currency_rates_data[1]
+        else:
+            last_available_rate = self.__get_first_available_rate_from_date(date_from, currency)
+            rate_date = date_from
+            currency_rates = []
+            while rate_date <= date_to:
+                currency_rates.append(last_available_rate)
+                interpolation_statements.append(True)
+                dates.append(rate_date)
+                rate_date += timedelta(days=1)
+
+        return self.__insert_currency_data(dates, currency_rates, interpolation_statements, table_name)
+
+    def __create_currency_rates_for_dates(self, currency_data, date_from, date_to):
+        rates_with_interpolation = self.__get_rates_and_interpolation_from_currency_data(currency_data, date_from)
+        currency_rates = rates_with_interpolation[0]
+        interpolation_statements = rates_with_interpolation[1]
+        last_rate = currency_rates[-1]
+        last_date = datetime.strptime(currency_data[0][-1][0], '%Y-%m-%d')
+        if last_date != date_to:
+            missing_data = self.__create_interpolated_data(last_date,
+                                                           datetime.combine(date_to, datetime.min.time()),
+                                                           last_rate)
+            currency_rates += missing_data[0]
+            interpolation_statements += missing_data[1]
+
+        return currency_rates, interpolation_statements
+
+    def __get_rates_and_interpolation_from_currency_data(self, currency_data, date_from):
         currency_rates = []
+        interpolation_statements = []
         last_date = datetime.strptime(currency_data[0][0][0], '%Y-%m-%d')
         last_rate = self.__get_first_available_rate_from_date(last_date, currency_data[1])
-
+        first_iteration = True
         for data in currency_data[0]:
             rate_date = datetime.strptime(data[0], '%Y-%m-%d')
-            delta = rate_date - last_date
+            if first_iteration and rate_date != date_from:
+                missing_data = self.__create_interpolated_data(datetime.combine(date_from, datetime.min.time()),
+                                                               rate_date - timedelta(days=1),
+                                                               last_rate)
+                currency_rates += missing_data[0]
+                interpolation_statements += missing_data[1]
+                first_iteration = False
 
+            delta = rate_date - last_date
             if delta.days > 1:
                 for days_diff in range(delta.days - 1):
                     currency_rates.append(last_rate)
+                    interpolation_statements.append(True)
                 currency_rates.append(data[1])
+                interpolation_statements.append(False)
             else:
                 currency_rates.append(data[1])
+                interpolation_statements.append(False)
             last_rate = data[1]
             last_date = rate_date
-        return currency_rates
+        return currency_rates, interpolation_statements
+
+    @staticmethod
+    def __create_interpolated_data(date_from, date_to, rate):
+        rate_date = date_from
+        currency_rates = []
+        interpolation_statements = []
+        while rate_date <= date_to:
+            currency_rates.append(rate)
+            interpolation_statements.append(True)
+            rate_date += timedelta(days=1)
+        return currency_rates, interpolation_statements
 
     def __get_first_available_rate_from_date(self, rate_date, currency):
         currency_data_downloader = CurrencyDataDownloader(currency)
         last_available_rate = 0
         last_available_rate_date = rate_date - timedelta(days=1)
         while last_available_rate == 0 and last_available_rate_date.year >= self.__min_available_year:
-            rate_data = currency_data_downloader.\
+            rate_data = currency_data_downloader. \
                 get_currency_prices_for_date(datetime.strftime(last_available_rate_date, '%Y-%m-%d'))
             if len(rate_data[0]) > 0 and rate_data[0][0] is not None:
                 last_available_rate = rate_data[0][0][1]
@@ -98,13 +133,13 @@ class DatabaseManager:
 
         return last_available_rate
 
-    def __insert_currency_data(self, dates, currency_rates, table_name):
+    def __insert_currency_data(self, dates, currency_rates, interpolation_statements, table_name):
         if table_name is not None:
             for i in range(len(dates)):
                 self.__cursor.execute(f'''
-                    INSERT INTO {table_name} (QuotationDate, CurrencyRate)
-                    VALUES (?,?)''',
-                                      dates[i], currency_rates[i])
+                    INSERT INTO {table_name} (QuotationDate, CurrencyRate, Interpolated)
+                    VALUES (?,?,?)''',
+                                      dates[i], currency_rates[i], interpolation_statements[i])
             self.__conn.commit()
             return True
         return False
@@ -116,16 +151,12 @@ class DatabaseManager:
         else:
             return None
 
-    def delete_table(self, table_name):
-        self.__cursor.execute(f"DROP TABLE {table_name};")
-        self.__conn.commit()
-
     def get_currency_rates(self, currency, date_from, date_to):
         table_name = self.__get_table_name_for_currency(currency)
         is_table_created = self.__check_currency_table_name_and_create(table_name)
         if is_table_created:
             self.__cursor.execute(
-                f"SELECT QuotationDate, CurrencyRate FROM {table_name} WHERE QuotationDate "
+                f"SELECT * FROM {table_name} WHERE QuotationDate "
                 f"BETWEEN '{date_from}' AND '{date_to}' ORDER BY QuotationDate")
             rates = []
             for row in self.__cursor:
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def parse_str_to_date(str_date):
 
 
 if __name__ == '__main__':
-    str_date_from = "2020-11-07"
-    str_date_to = "2020-11-08"
+    str_date_from = "2019-11-05"
+    str_date_to = "2019-11-12"
 
     date_from = parse_str_to_date(str_date_from)
     date_to = parse_str_to_date(str_date_to)
-    currency_api.get_currency_rates('USD', date_from, date_to)
+    currency_api.get_currency_rates('GBP', date_from, date_to)
