8542e6dfcb410152b56bd1995175c6afd41f6bc2
limisie <k.limisiewicz@gmail.com>
2020-12-20 20:25:24 +0100

    Fix minor bugs views.py

--- L5_API/views.py
+++ L5_API/views.py
@@ -14,7 +14,7 @@ def get_last_rate(code):
     date = db_app.get_last_date(code)
     rate, ipd = db_app.get_rate(code, date)
 
-    return {"Currencycode": code, "Rates": {"Rate": {"Date": date, "Rate": rate, "Interpolated": ipd}}}
+    return {"Currency Code": code, "Rates": {"Rate": {"Date": date, "Rate": rate, "Interpolated": ipd}}}
 
 
 def get_sale(date):
@@ -70,7 +70,7 @@ def __sales_serializer(data):
 
 
 def __rates_serializer(code, data):
-    output = {"Currencycode": code.upper(), "Rates": {}}
+    output = {"Currency Code": code.upper(), "Rates": {}}
     for index, d in enumerate(data, start=1):
         output["Rates"][index] = {"Date": d["date"], "Rate": d["rate"], "Interpolated": d["ipd"]}
     return output
@@ -104,10 +104,10 @@ def __are_dates(date_from, date_to):
 
 
 def __are_dates_chronological(date_from, date_to):
-    return date_from < date_to
+    return date_from <= date_to
 
 
-def __are_in_range(date_from, date_to, code):
+def __are_in_limit(date_from, date_to, code):
     if code == 'NONE':
         date_min, date_max = db_app.get_sales_limits()
     else:
@@ -116,10 +116,10 @@ def __are_in_range(date_from, date_to, code):
     date_min = datetime.strptime(date_min, DATE_FORMAT).date()
     date_max = datetime.strptime(date_max, DATE_FORMAT).date()
 
-    return date_to > date_min or date_to < date_max or date_from > date_min or date_from < date_max
+    return date_min <= date_to <= date_max and date_min <= date_from <= date_max
 
 
-def __are_in_limit(date_from, date_to):
+def __are_in_range(date_from, date_to):
     return (date_to - date_from).days < DATA_LIMIT
 
 
@@ -129,7 +129,7 @@ def __validate_date(date, code='NONE'):
 
     date = datetime.strptime(date, DATE_FORMAT).date()
 
-    if not __are_in_range(date, date, code):
+    if not __are_in_limit(date, date, code):
         return False, '400 BadRequest - Invalid date range - date outside the database limit', 400
 
     return True, '', 200
@@ -145,10 +145,10 @@ def __validate_dates(date_from, date_to, code='NONE'):
     if not __are_dates_chronological(date_from, date_to):
         return False, '400 BadRequest - Invalid date range - endDate is before startDate', 400
 
-    if not __are_in_range(date_from, date_to, code):
+    if not __are_in_limit(date_from, date_to, code):
         return False, '400 BadRequest - Invalid date range - date outside the database limit', 400
 
-    if not __are_in_limit(date_from, date_to):
+    if not __are_in_range(date_from, date_to):
         return False, '400 BadRequest - Limit of {} days has been exceeded'.format(DATA_LIMIT), 400
 
     return True, '', 200
