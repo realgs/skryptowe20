141a7950f810999a2801959a728b14679ffae658
maciej.wasilewski <wasilewski078@gmail.com>
2020-12-20 18:50:07 +0100

    Fixed batch script - it works now as intended

--- start.bat
+++ start.bat
@@ -1,13 +1,11 @@
 @ECHO OFF
 IF EXIST venv (
     venv\Scripts\activate
-    python app.py --update
-     
+    python app.py --update --run
 ) ELSE (
     python -m venv venv
     venv\Scripts\activate
     python -m pip install --upgrade pip
-    pip install requests, pandas, flask, flask-caching, flask-limiter
-    python app.py --set-up
+    pip install requests pandas flask flask-caching flask-limiter
+    python app.py --set-up --run
 )
-python app.py --run
