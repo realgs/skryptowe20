0f5db365b6d8fb0def799946018f7ea1317ec567
Norbert Nawrotek <norbertn15@gmail.com>
2020-12-20 14:51:23 +0100

    Rename queries

--- app.py
+++ app.py
@@ -67,18 +67,18 @@ def api_single_sum_of_transaction(currency, date):
 
     currency = currency.upper()
 
-    select_single_exchange_rate_query = ""
+    select_single_sum_of_transaction_query = ""
 
     if currency == "PLN":
-        select_single_exchange_rate_query = """SELECT ST.date, ST.pln_value FROM SumOfTransaction ST 
+        select_single_sum_of_transaction_query = """SELECT ST.date, ST.pln_value FROM SumOfTransaction ST 
         WHERE ST.date = ?;"""
     elif currency == "USD":
-        select_single_exchange_rate_query = """SELECT ST.date, ST.usd_value FROM SumOfTransaction ST 
+        select_single_sum_of_transaction_query = """SELECT ST.date, ST.usd_value FROM SumOfTransaction ST 
                 WHERE ST.date = ?;"""
     else:
         abort(404, "Wrong currency")
 
-    result = get_result_from_query(select_single_exchange_rate_query, (string_date,))
+    result = get_result_from_query(select_single_sum_of_transaction_query, (string_date,))
 
     return jsonify(result)
 
@@ -93,18 +93,18 @@ def api_multiple_sum_of_transaction(currency, start_date, end_date):
 
     currency = currency.upper()
 
-    select_single_exchange_rate_query = ""
+    select_single_sum_of_transaction_query = ""
 
     if currency == "PLN":
-        select_single_exchange_rate_query = """SELECT ST.date, ST.pln_value FROM SumOfTransaction ST 
+        select_single_sum_of_transaction_query = """SELECT ST.date, ST.pln_value FROM SumOfTransaction ST 
         WHERE ST.date >= ? AND ST.DATE <= ?;"""
     elif currency == "USD":
-        select_single_exchange_rate_query = """SELECT ST.date, ST.usd_value FROM SumOfTransaction ST 
+        select_single_sum_of_transaction_query = """SELECT ST.date, ST.usd_value FROM SumOfTransaction ST 
                 WHERE ST.date >= ? AND ST.DATE <= ?;"""
     else:
         abort(404, "Wrong currency")
 
-    result = get_result_from_query(select_single_exchange_rate_query, (string_start_date, string_end_date))
+    result = get_result_from_query(select_single_sum_of_transaction_query, (string_start_date, string_end_date))
 
     return jsonify(result)
 
