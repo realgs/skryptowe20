a5e1192e6269f02b4ca1ec4e2b0b05f14a485cd4
Mateusz Groblicki <lokigameplayer@gmail.com>
2020-12-20 10:28:47 +0100

    added exceptions and basic exception handling

--- lab5-api/app.py
+++ lab5-api/app.py
@@ -3,6 +3,7 @@
 from typing import Dict
 import flask
 from flask import Flask
+from werkzeug.exceptions import InternalServerError
 from config import *
 from sqlite3 import DatabaseError
 from flask_limiter import Limiter
@@ -11,32 +12,83 @@ from dbhandler import Currency
 import caching
 
 
-app = Flask(__name__)
+############    EXCEPTIONS      ##############
+class DateFormatError(Exception):
+    pass
+
+
+class OutOfRangeError(Exception):
+    pass
+
+
+class CurrencyCodeError(Exception):
+    pass
+
+
+class NoCurrencyError(Exception):
+    pass
+
+############    MISC    ##############
+def validate_currency_code(code: str) -> bool:
+    return code.isalpha() and len(code) == 3
+
 
 sales_sum_precached: Dict[Currency, Dict[dt.date, float]] = {}
 if ENABLE_PRECACHING:
     sales_sum_precached[Currency.UNITED_STATES_DOLLAR] = caching.cache_sales_sum_original()
-    for curr in Currency:
-        if curr != Currency.UNITED_STATES_DOLLAR:
-            sales_sum_precached[curr] = caching.cache_sales_sum(
-                curr, sales_sum_precached[Currency.UNITED_STATES_DOLLAR])
+    sales_sum_precached[Currency.POLISH_ZLOTY] = caching.cache_sales_sum(
+        Currency.POLISH_ZLOTY, sales_sum_precached[Currency.UNITED_STATES_DOLLAR])
 
+
+############    API   #################
+
+
+app = Flask(__name__)
+
+
+############    ERROR HANDLING  ##############
 
 @app.errorhandler(DatabaseError)
 def db_exception_handler(error):
-    return 'Database connection failed', 500
+    return {'error': type(error).__name__, 'message': 'Database connection failed. Please contact server administrator.'}, 500
+
+
+@app.errorhandler(DateFormatError)
+def date_exception_handler(error):
+    return {'error': type(error).__name__, 'message': 'Invalid date argument. Date has to be passed in given format: YYYY-MM-DD'}, 422
+
+
+@app.errorhandler(OutOfRangeError)
+def date_exception_handler(error):
+    return {'error': type(error).__name__, 'message': 'Requested range exceedes the maximum range available in database.'}, 422
+
+
+@app.errorhandler(CurrencyCodeError)
+def date_exception_handler(error):
+    return {'error': type(error).__name__, 'message': 'Invalid currency code. Valid code should consist of 3 letters'}, 422
+
+
+@app.errorhandler(NoCurrencyError)
+def date_exception_handler(error):
+    return {'error': type(error).__name__, 'message': 'Requested currency is not available in database.'}, 422
+
+
+@app.errorhandler(InternalServerError)
+def handle_500(error):
+    return {'error': type(error).__name__, 'message': '500 Internal Server Error'}, 500
 
 
 ############    ROUTING     ####################
 
 @app.route('/')
 def index():
-    pass
+    return ''
 
 
 @app.route('/api/v1/rates/<currency_code>/all')
 def get_rates_all(currency_code: str):
-    pass
+    if not validate_currency_code(currency_code):
+        raise CurrencyCodeError
 
 
 @app.route('/api/v1/rates/<currency_code>/day/<date>')
@@ -49,8 +101,8 @@ def get_rates_range(currency_code: str, start_date: str, end_date: str):
     pass
 
 
-@app.route('/api/v1/sales/sum/<currency_code>/<date>')
-def get_sales_sum_day(currency_code: str, date: str):
+@app.route('/api/v1/sales/sum/<date>')
+def get_sales_sum_day(date: str):
     pass
 
 
