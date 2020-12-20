f47c471730bd0f0811f4ced04c2ef5441e52ae5f
Norbert Nawrotek <norbertn15@gmail.com>
2020-12-20 14:15:41 +0100

    Add query for selecting sum of transactions per day

--- table_manipulator.py
+++ table_manipulator.py
@@ -8,15 +8,19 @@ MAX_DATE = datetime.date(2016, 2, 29)
 CREATE_TABLE_QUERY = "CREATE TABLE ExchangeRate (date text, rate real, is_interpolated boolean)"
 INSERT_QUERY = "INSERT INTO ExchangeRate (date, rate, is_interpolated) VALUES (?, ?, ?)"
 DROP_TABLE_QUERY = "DROP TABLE ExchangeRate"
-SELECT_QUERY = """SELECT ER.date, ER.rate, ROUND(orderSum, 2)
-FROM ExchangeRate ER LEFT JOIN 
+SELECT_SUM_OF_TRANSACTIONS_QUERY = """
+SELECT 
+ER.date transaction_date, ROUND(IFNULL(orderSum, 0), 2) usd_value, ROUND(IFNULL(orderSum, 0) * ER.rate, 2) pln_value 
+FROM 
+ExchangeRate ER LEFT JOIN 
 (
 SELECT DATE(O.OrderDate) innerDate, SUM(OD.UnitPrice * OD.Quantity) orderSum
 FROM OrderDetail OD JOIN `Order` O ON OD.OrderId = O.Id
 GROUP BY DATE(O.OrderDate)
 ) ON ER.date = innerDate
 ORDER BY
-ER.date;"""
+ER.date;
+"""
 
 
 def get_rows_from_api(currency_code, start_date, end_date):
