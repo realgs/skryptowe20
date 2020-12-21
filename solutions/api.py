b9fa0afd48da5c4a9130b1c2260e4184574ae0bf
Robert <robertpawelh@gmail.com>
2020-12-21 13:29:58 +0100

    Make running easier

--- solutions/api.py
+++ solutions/api.py
@@ -52,4 +52,4 @@ async def read_transaction_sum_in_day(request: Request, date: datetime.date):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
