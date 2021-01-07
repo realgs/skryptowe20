4f456801873a8b4a405e24689d647bedf746e755
pbeat8 <61192446+pbeat8@users.noreply.github.com>
2021-01-07 18:12:36 +0100

    add nbp.py back, edit limits

--- lab5/api.py
+++ lab5/api.py
@@ -4,17 +4,20 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import db
 
+DATABASE = r"C:\Users\Patrycja\Desktop\5 semestr\Języki skryptowe\salesData.db"
 MINDATE = '2013-01-04'
 MAXDATE = '2016-12-29'
-SALES = []
 
 app = flask.Flask(__name__)
+# limit na osobe po ip
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    default_limits=["300 per day", "20 per minute"]
+    default_limits=["300 per day", "20 per minute"],
+    application_limits=["10000 per day", "3 per minute", "10 per second"]
 )
 app.config["DEBUG"] = True
+SALES = []
 
 
 @app.route('/', methods=['GET'])
@@ -24,8 +27,8 @@ def home():
            "completed with data for missing ratings. You can also access total sales data from my database</p>"
 
 
-@app.route('/api/rates/<code>/<date>', methods=['GET'])
-def api_rates_date(code, date):
+@app.route('/api/rates/USD/<date>', methods=['GET'])
+def api_rates_usd_date(date):
     if date < MINDATE or date > MAXDATE:
         return 'ERROR: Given date is outside of supported range. ' \
                'Supported dates are from {} to {}'.format(MINDATE, MAXDATE)
@@ -34,15 +37,27 @@ def api_rates_date(code, date):
         result = db.get_rate(conn, date)
     if len(result) == 0:
         return 'ERROR: No data found'
-    if code == 'PLN':
-        for elem in result:
-            val = 1 / elem['Value']
-            elem['Value'] = val
     return jsonify(result)
 
 
-@app.route('/api/rates/<code>/<startdate>/<enddate>', methods=['GET'])
-def api_rates_timespan(code, startdate, enddate):
+@app.route('/api/rates/PLN/<date>', methods=['GET'])
+def api_rates_pln_date(date):
+    if date < MINDATE or date > MAXDATE:
+        return 'ERROR: Given date is outside of supported range. ' \
+               'Supported dates are from {} to {}'.format(MINDATE, MAXDATE)
+    conn = db.create_connection()
+    with conn:
+        result = db.get_rate(conn, date)
+    for elem in result:
+        val = 1 / elem['Value']
+        elem['Value'] = val
+    if len(result) == 0:
+        return 'ERROR: No data found'
+    return jsonify(result)
+
+
+@app.route('/api/rates/USD/<startdate>/<enddate>', methods=['GET'])
+def api_rates_usd_timespan(startdate, enddate):
     if startdate > enddate:
         return 'ERROR: Invalid dates'
     if (startdate < MINDATE and enddate < MINDATE) or (startdate > MAXDATE and enddate > MAXDATE):
@@ -55,10 +70,26 @@ def api_rates_timespan(code, startdate, enddate):
     conn = db.create_connection()
     with conn:
         result = db.get_multiple_rates(conn, startdate, enddate)
-    if code == 'PLN':
-        for elem in result:
-            val = 1 / elem['Value']
-            elem['Value'] = val
+    return jsonify(result)
+
+
+@app.route('/api/rates/PLN/<startdate>/<enddate>', methods=['GET'])
+def api_rates_pln_timespan(startdate, enddate):
+    if startdate > enddate:
+        return 'ERROR: Invalid dates'
+    if (startdate < MINDATE and enddate < MINDATE) or (startdate > MAXDATE and enddate > MAXDATE):
+        return 'ERROR: Given time span is outside of supported range. ' \
+               'Supported dates are from {} to {}'.format(MINDATE, MAXDATE)
+    if startdate < MINDATE:
+        startdate = MINDATE
+    if enddate > MAXDATE:
+        enddate = MAXDATE
+    conn = db.create_connection()
+    with conn:
+        result = db.get_multiple_rates(conn, startdate, enddate)
+    for elem in result:
+        val = 1 / elem['Value']
+        elem['Value'] = val
     return jsonify(result)
 
 
@@ -80,7 +111,7 @@ def api_sales(date):
         if elem['Date'] == date:
             result.append(elem)
     if len(result) == 0:
-        return 'ERROR: No data found. Sales on given days totalled 0'
+        return 'ERROR: No data found. Sales on given day totalled 0'
     return jsonify(result)
 
 
