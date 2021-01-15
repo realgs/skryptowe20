0eebd5709f4c566011dcb8bf5e4dc6e7f6af3100
DamianBartosz <damian.mateusz.bartosz@gmail.com>
2021-01-15 14:04:24 +0100

    Init for heroku app

---
+++ server/api.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+from flask import jsonify
+from server.app_config import *
+from server.db_models import *
+from server.api_exceptions import *
+import os
+
+dateFormat = '%Y-%m-%d'
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/rates/<code>/<dateStr>', methods=['GET'])
+def rateInDate(code, dateStr):
+    try:
+        datetime.strptime(dateStr, dateFormat)
+    except Exception:
+        raise BadRequest()
+    exchangeRate = Exchange.objects(code=code.upper(), dateStr=dateStr)
+    if len(exchangeRate) == 0:
+        raise NoDataFound('No data found with code: {} and date: {}'.format(code.upper(), dateStr))
+    return jsonify(exchangeRate[0])
+
+
+@app.route('/rates/<code>/<startDateStr>/<endDateStr>', methods=['GET'])
+@cache.memoize(600)
+def ratesBetweenDates(code, startDateStr, endDateStr):
+    try:
+        startDate = datetime.strptime(startDateStr, dateFormat)
+        endDate = datetime.strptime(endDateStr, dateFormat)
+    except Exception:
+        raise BadRequest()
+    if startDate > endDate:
+        raise BadRequest()
+    exchangeRates = Exchange.objects(code=code.upper()).filter((Q(date__gte=startDate) & Q(date__lte=endDate)))
+    if len(exchangeRates) == 0:
+        raise NoDataFound(
+            'No data found with code: {} and date range: <{}, {}>'.format(code.upper(), startDateStr, endDateStr))
+    return jsonify(exchangeRates)
+
+
+@app.route('/sales/<dateStr>', methods=['GET'])
+def saleInDate(dateStr):
+    try:
+        datetime.strptime(dateStr, dateFormat)
+    except Exception:
+        raise BadRequest()
+    sale = SalesResult.objects(dateStr=dateStr)
+    if len(sale) == 0:
+        raise NoDataFound('No data found with date: {}'.format(dateStr))
+    return jsonify(sale[0])
+
+
+@app.route('/sales/<startDateStr>/<endDateStr>', methods=['GET'])
+@cache.memoize(600)
+def salesBetweenDates(startDateStr, endDateStr):
+    try:
+        startDate = datetime.strptime(startDateStr, dateFormat)
+        endDate = datetime.strptime(endDateStr, dateFormat)
+    except Exception:
+        raise BadRequest()
+    if startDate > endDate:
+        raise BadRequest()
+    sales = SalesResult.objects().filter((Q(date__gte=startDate) & Q(date__lte=endDate)))
+    if len(sales) == 0:
+        raise NoDataFound(
+            'No data found in date range: <{}, {}>'.format(startDateStr, endDateStr))
+    return jsonify(sales)
+
+
+@app.errorhandler(ApiException)
+def handle_invalid_usage(error):
+    return error.to_dict(), error.status_code
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
---
+++ server/app_config.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_caching import Cache
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = MongoEngine()
+
+app = Flask(__name__,static_folder='../client/public/',    static_url_path='/')
+config = {
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+app.config.from_mapping(config)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'currency-and-sales',
+    'host': "mongodb+srv://Skryptowe:yArfxRUIvpFQii7p@cluster0.gcuoh.mongodb.net/currency-and-sales?retryWrites=true"
+            "&w=majority "
+}
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour, 1/second"]
+)
+db.init_app(app)
+cache = Cache(app)
