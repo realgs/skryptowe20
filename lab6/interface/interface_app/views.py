b6e71def045d02b300c188d2bdaa9383054998bf
Jacek <jacek.szelewski@gmail.com>
2021-01-20 18:03:48 +0100

    Add displaying exchange rates

--- lab6/interface/interface_app/views.py
+++ lab6/interface/interface_app/views.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
 
 
 # Create your views here.
@@ -8,4 +11,25 @@ def home(request):
 
 
 def request_builder(request):
-    return render(request, "interface_app/request_builder.html")
+    return show_exchange_rates(request, datetime.date(year=2009, month=2, day=2),
+                        datetime.date(year=2009, month=5, day=24), 'USD')
+
+
+def show_exchange_rates(request, start_date, end_date, currency_code):
+    start_date_string = __format_date(start_date)
+    end_date_string = __format_date(end_date)
+    request_url = 'http://127.0.0.1:5000/api/exchangerate/{0}/{1}/{2}'.format(currency_code, start_date_string,
+                                                                              end_date_string)
+    response = requests.get(request_url)
+    other_currency_code = 'PLN' if currency_code == 'USD' else 'USD'
+
+    return render(request, "interface_app/display_exchange_rates.html",
+                  context={'exchange_rates_json': response.json(),
+                           'other_currency_code': other_currency_code,
+                           'start_date': start_date_string,
+                           'end_date': end_date_string,
+                           })
+
+
+def __format_date(date):
+    return date.strftime('%Y-%m-%d')
