413a546b3002ed13affc8e64182c2ed83cecd215
Jacek <jacek.szelewski@gmail.com>
2021-01-21 10:50:18 +0100

    Add displaying exchange rates plot

--- lab6/interface/interface_app/views.py
+++ lab6/interface/interface_app/views.py
@@ -13,13 +13,13 @@ def home(request):
 def request_builder(request):
     if request.method == 'POST':
         if request.POST['select-value'] == 'exchange-rates':
-            #użytkownik chce kurs walut
+            # użytkownik chce kurs walut
             if request.POST['select-date'] == 'date-range':
                 start_date = request.POST['date-range-start']
                 end_date = request.POST['date-range-end']
             else:
                 start_date = end_date = request.POST['single-date-input']
-            return show_exchange_rates(request, start_date, end_date, request.POST['currencies'])
+            return show_exchange_rates(request, start_date, end_date, request.POST['currencies'], True)
         else:
             # użytkownik chce podsumowanie sprzedaży
             if request.POST['select-date'] == 'date-range':
@@ -32,13 +32,18 @@ def request_builder(request):
     return render(request, "interface_app/request_builder.html")
 
 
-def show_exchange_rates(request, start_date, end_date, currency_code):
+def show_exchange_rates(request, start_date, end_date, currency_code, is_chart):
     request_url = 'http://127.0.0.1:5000/api/exchangerate/{0}/{1}/{2}'.format(currency_code, start_date,
                                                                               end_date)
     response = requests.get(request_url)
     other_currency_code = 'PLN' if currency_code == 'USD' else 'USD'
 
-    return render(request, "interface_app/display_exchange_rates.html",
+    if is_chart:
+        template_name = "interface_app/display_exchange_rates_chart.html"
+    else:
+        template_name = "interface_app/display_exchange_rates.html"
+
+    return render(request, template_name,
                   context={'exchange_rates_json': response.json(),
                            'other_currency_code': other_currency_code,
                            'start_date': start_date,
