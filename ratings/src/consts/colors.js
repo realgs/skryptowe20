a4944f7f68b88faca22a1f2727a273d3de70367e
Pawel <mrprzyprawa@gmail.com>
2021-01-22 00:05:52 +0100

    Update colors

--- ratings/src/consts/colors.js
+++ ratings/src/consts/colors.js
@@ -1,7 +1,8 @@
 export const colors = {
-  white: "#F2F2F2",
-  light: "#FCE7E4",
-  primary: "#F2D3D0",
-  darker: "#F8CDC5",
-  dark: "#F3A99B",
+  white: "#FEFEFE",
+  light: "#D168FF",
+  primaryLight: "#B850FF",
+  primary: "#A240FC",
+  primaryDark: "#8D18FF",
+  dark: "#6A0DC4",
 };
