38e345f6b016e86b92c1eb9c25e34258b36d0dd1
Grzegorz Myjak <g-myjak@wp.pl>
2021-01-21 22:58:42 +0100

    Added routing, pages scaffolds and navbar

--- currency-api-project/front-end/src/App.js
+++ currency-api-project/front-end/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import Income from './pages/Income';
+import ApiInfo from './pages/ApiInfo';
+import Rates from './pages/Rates';
+import Error from './pages/Error';
+import {Navbar, Nav} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand href="/" style={{color: 'white'}}>Currency api</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="rates" style={{color: 'white'}}>Rates</Nav.Link>
+            <Nav.Link href="income" style={{color: 'white'}}>Income</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Router>
+        <Switch>
+        <Route path='/' exact component={ApiInfo} />
+        <Route path='/rates'component={Rates} />
+        <Route path='/income' component={Income} />
+        <Route path='/' component={Error} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ currency-api-project/front-end/src/pages/Rates.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Rates() 
+{
+    return (
+        <div>
+            <h1>Rates</h1>
+        </div>
+    )
+}
