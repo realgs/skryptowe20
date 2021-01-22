85e6bf7efae6bac2ad5708a3ae4c2b5bde5c46a7
Pawel <mrprzyprawa@gmail.com>
2021-01-22 22:10:50 +0100

    Update colors

--- ratings/src/consts/colors.js
+++ ratings/src/consts/colors.js
@@ -5,4 +5,8 @@ export const colors = {
   primary: "#A240FC",
   primaryDark: "#8D18FF",
   dark: "#6A0DC4",
+  grey: "#eee",
+  navbarBgColor: "hsl(0, 0%, 15%)",
+  navbarTextColor: "hsl(0, 0%, 85%)",
+  navbarBgContrast: "hsl(0, 0%, 25%)",
 };
