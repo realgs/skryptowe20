96ec2a597fba4b2f9506246e04d89ebedc245403
Norbert Nawrotek <norbertn15@gmail.com>
2021-01-22 22:38:40 +0100

    Add table

--- static/js/api_handler.js
+++ static/js/api_handler.js
@@ -1,11 +1,13 @@
 let apiLink = document.getElementById('api_link');
 let responseArea = document.getElementById('response_value');
+let responseTable = document.getElementById('response_table');
 let apiTypeDropdown = document.getElementById('api_type_dropdown');
 let currencyDropdown = document.getElementById('currency_dropdown');
 let singleDateOnlyCheckbox = document.getElementById('single_date_only_checkbox');
 let singleDate = document.getElementById('single_date');
 let startDate = document.getElementById('start_date');
 let endDate = document.getElementById('end_date');
+
 
 async function generateLink() {
     let datesPart = getDatesPart();
@@ -18,6 +20,8 @@ async function generateLink() {
     const response = await fetch(link);
     const jsonResponse = await response.json()
     responseArea.value = JSON.stringify(jsonResponse);
+
+    fillTableWith(jsonResponse);
 }
 
 function getDatesPart() {
@@ -34,4 +38,37 @@ function getApiTypePart() {
     } else {
         return `${apiTypeDropdown.value}`
     }
+}
+
+function fillTableWith(jsonResponse) {
+    let table_array = jsonResponse['result'];
+
+    let cols = Object.keys(table_array[0]);
+
+    let headerRow;
+
+    if (cols.length === 3) {
+        headerRow = '<th>Date</th><th>Exchange rate</th><th>Is interpolated?</th>'
+    } else {
+        headerRow = '<th>Date</th><th>Sum</th>'
+    }
+
+    let rows = table_array
+        .map(row => {
+            let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
+            return `<tr>${tds}</tr>`;
+        })
+        .join("");
+
+    const table = `
+	<table>
+		<thead>
+			<tr>${headerRow}</tr>
+		<thead>
+		<tbody>
+			${rows}
+		<tbody>
+	<table>`;
+
+    responseTable.innerHTML = table;
 }
