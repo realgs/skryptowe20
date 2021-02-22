dc09b07bd8deff481542a564a14051ec04acfd74
Silkypaladin <bednarski.szymon99@gmail.com>
2021-02-22 19:50:54 +0100

    Add postgres config

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app:app
+DATABASE_URL=$(heroku config:get DATABASE_URL -a salesman-api) python app.py
--- api/manage_db.py
+++ api/manage_db.py
@@ -1,7 +1,9 @@
 import sqlite3
 from flask import g
+import psycopg2, os
 
 DB_NAME = 'data.db'
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -16,10 +18,11 @@ def close_db(e=None):
         db.close()
 
 def create_exchange_rates_table():
-    conn = sqlite3.connect(DB_NAME)
+    #conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DATABASE_URL)
     cursor = conn.cursor()
     cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS rates(date text, rate real, interpolated boolean)''')
+        '''CREATE TABLE IF NOT EXISTS rates(date VARCHAR(255), rate REAL, interpolated BOOLEAN)''')
     conn.commit()
     conn.close()
 
@@ -28,14 +31,16 @@ def populate_exchange_rates_table(rates_and_dates_interpolated):
     if len(rates_and_dates_interpolated) < 1:
         print("Incorrect data")
         return
-    conn = sqlite3.connect(DB_NAME)
+    #conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DATABASE_URL)
     cursor = conn.cursor()
     cursor.executemany("INSERT INTO rates VALUES (?, ?, ?)", rates_and_dates_interpolated)
     conn.commit()
     conn.close()
 
 def get_transaction_sums_for_days(years):
-    conn = sqlite3.connect(DB_NAME)
+    #conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DATABASE_URL)
     cursor = conn.cursor()
     sales_data = cursor.execute(
         "SELECT SUM(sales), orderdate FROM sales GROUP BY orderdate HAVING year_id IN ({seq}) ORDER BY year_id;".format(seq=','.join(['?']*len(years))), years).fetchall()
@@ -43,7 +48,8 @@ def get_transaction_sums_for_days(years):
     return sales_data
 
 def get_exchange_rates_for_days(days):
-    conn = sqlite3.connect(DB_NAME)
+    #conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DATABASE_URL)
     cursor = conn.cursor()
     rates = cursor.execute(
         "SELECT rate, interpolated FROM rates GROUP BY date HAVING date IN ({seq}) ORDER BY date;".format(seq=','.join(['?']*len(days))), days).fetchall()
@@ -51,15 +57,17 @@ def get_exchange_rates_for_days(days):
     return rates
 
 def check_if_table_exists():
-    conn = sqlite3.connect(DB_NAME)
+    #conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DATABASE_URL)
     cursor = conn.cursor()
     exists = cursor.execute(
-        '''SELECT name FROM sqlite_master WHERE type='table' AND name='rates';''').fetchone()
+        '''SELECT name FROM information_schema.tables table_name='rates';''').fetchone()
     conn.close()
     return exists != None
 
 def drop_exchange_rates_table():
-    conn = sqlite3.connect(DB_NAME)
+    #conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(DATABASE_URL)
     cursor = conn.cursor()
     cursor.execute(
         '''DROP TABLE IF EXISTS rates''')
