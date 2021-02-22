b5432e30729df257e8dbf251e5d60ebb39dd89ad
Silkypaladin <bednarski.szymon99@gmail.com>
2021-02-22 19:31:11 +0100

    Prepare deploy files

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from api import create_app
 import api.config
 
 
+app = create_app(api.config.DevelopmentConf)
 if __name__ == "__main__":
-    app = create_app(api.config.DevelopmentConf)
     app.run()
